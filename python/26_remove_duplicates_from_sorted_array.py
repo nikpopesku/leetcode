6b01c41e36644b353fabc11e86e6072f47be3219
npopesku <nikolay.popesku@unbiased.co.uk>
2022-02-20 21:24:51 +0300

    created python folder

--- python/26_remove_duplicates_from_sorted_array.py
+++ python/26_remove_duplicates_from_sorted_array.py
@@ -1,6 +1,21 @@
+import sys
 from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        pass
+        previous = None
+        unique = 0
 
+        for element in nums:
+            if element != previous:
+                unique += 1
+            elif previous == None:
+                previous = element
+                nums[unique] = element
+                unique += 1
+
+        return unique
+
+
+solution = Solution()
+print(solution.removeDuplicates([int(x) for x in sys.argv[1:]]))
