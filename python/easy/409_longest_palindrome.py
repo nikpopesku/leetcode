06fb1ae1c212d5dbf6fe56a3b20150b2bae44f08
npopesku <nikolay.popesku@unbiased.co.uk>
2022-02-21 15:45:26 +0300

    409-longest-palindrome

--- python/easy/409_longest_palindrome.py
+++ python/easy/409_longest_palindrome.py
@@ -3,19 +3,17 @@ import sys
 
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        left = []
         right = []
         without_pair = {}
         for key in s:
             if key not in without_pair:
                 without_pair[key] = key
             else:
-                left.insert(0, key)
                 right.append(key)
                 del without_pair[key]
 
 
-        return len(left) + (1 if len(without_pair) >= 1 else 0) + len(right)
+        return (1 if len(without_pair) >= 1 else 0) + 2 * len(right)
 
 
 
