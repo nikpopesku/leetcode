73f6faa329af087aaf00f7bdae3e7797b4a974bb
npopesku <nikolay.popesku@unbiased.co.uk>
2022-04-08 22:55:34 +0300

    217_contains_duplicate

---
+++ python/data_structure_1/217_contains_duplicate.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+solution = Solution()
+print(solution.containsDuplicate([1,2,3,1]))
