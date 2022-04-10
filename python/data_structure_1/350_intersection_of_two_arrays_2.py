4946e649812de04a4db9f5095f8dff5671f966b8
npopesku <nikolay.popesku@unbiased.co.uk>
2022-04-10 17:18:24 +0300

    350_intersection_of_two_arrays_2

--- python/data_structure_1/350_intersection_of_two_arrays_2.py
+++ python/data_structure_1/350_intersection_of_two_arrays_2.py
@@ -3,7 +3,21 @@ from typing import List
 
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pass
+        hash = {}
+        response = []
+
+        for key, num in enumerate(nums1):
+            hash[num] = (hash[num] if num in hash else 0) + 1
+
+        for key, num in enumerate(nums2):
+            if num in hash and hash[num] > 0:
+                response.append(num)
+                hash[num] -= 1
+
+
+        return response
+
+
 
 solution = Solution()
-print(solution.intersect(nums1 = [1,2,2,1], nums2 = [2,2]))
+print(solution.intersect(nums1 = [4,9,5], nums2 = [9,4,9,8,4]))
