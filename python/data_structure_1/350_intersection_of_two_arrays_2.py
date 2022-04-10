02002f99542ce3f3a24a51785ea5deaa7fa0d8ea
npopesku <nikolay.popesku@unbiased.co.uk>
2022-04-10 16:55:03 +0300

    350_intersection_of_two_arrays_2

---
+++ python/data_structure_1/350_intersection_of_two_arrays_2.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        pass
+
+solution = Solution()
+print(solution.intersect(nums1 = [1,2,2,1], nums2 = [2,2]))
