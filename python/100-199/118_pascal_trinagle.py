b1ade1403f3e8ab029d3fbd43ebe602ea888c65c
npopesku <nikolay.popesku@unbiased.co.uk>
2022-04-12 23:37:37 +0300

    118_pascal_triangle

--- python/100-199/118_pascal_trinagle.py
+++ python/100-199/118_pascal_trinagle.py
@@ -3,7 +3,19 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        return  []
+
+        response = []
+
+        for row in range(numRows):
+            element = []
+            for key in range(row):
+                if key == 0 or key == row - 1:
+                    element.append(1)
+                else:
+                    element.append(response[row-1][key] + response[row-1][key-1])
+            response.append(element)
+
+        return  response
 
 
 solution = Solution()
