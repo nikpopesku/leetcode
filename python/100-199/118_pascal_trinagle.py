9e4a77de02b39ebdf46f9f4233a972db7e743be8
npopesku <nikolay.popesku@unbiased.co.uk>
2022-04-12 23:27:52 +0300

    188_pascal triangle

---
+++ python/100-199/118_pascal_trinagle.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        return  []
+
+
+solution = Solution()
+print(solution.generate(numRows = 5))
