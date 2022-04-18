f0e891b90ef5be22b804d5371e1c9250db4b9e20
npopesku <nikolay.popesku@unbiased.co.uk>
2022-04-18 18:02:38 +0300

    21_merge_two_sorted_lists

---
+++ listnode.py
@@ -0,0 +1,24 @@
+def create_single_linked_list(arr):
+    next = None
+    for key, value in enumerate(reversed(arr)):
+        next = ListNode(value, next)
+
+    return next
+
+
+class ListNode:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+
+
+    def print(self):
+        nodes = []
+        nodes.append(str(self.val))
+        node = self
+
+        while node.next:
+            node = node.next
+            nodes.append(str(node.val))
+
+        print(" -> ".join(nodes))
