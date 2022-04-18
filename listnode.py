dc112f98068a68032e3214e1720ecb117150114c
npopesku <nikolay.popesku@unbiased.co.uk>
2022-04-18 18:33:45 +0300

    21_merge_two_sorted_lists

--- listnode.py
+++ listnode.py
@@ -7,7 +7,7 @@ def create_single_linked_list(arr):
 
 
 class ListNode:
-    def __init__(self, val=None, next=None):
+    def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
