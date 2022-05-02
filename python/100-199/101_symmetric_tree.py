3c3fd17e8c135f0115b6af879d017f142dcb4d55
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-02 17:21:45 +0300

    101_symmetric_tree

--- python/100-199/101_symmetric_tree.py
+++ python/100-199/101_symmetric_tree.py
@@ -4,16 +4,19 @@ from treenode import TreeNode
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         response = self.inorderTraversal(root.left) if root and root.left else []
+        if root and root.right and not root.left:
+            response += [None]
         response += [root.val] if root else []
         response += self.inorderTraversal(root.right) if root and root.right else []
+        if root and root.left and not root.right:
+            response += [None]
+
 
         return response
 
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         if not root:
             return True
-        elif root.left and not root.right or root.right and not root.left:
-            return False
         elif not root.left and not root.right:
             return True
 
@@ -25,10 +28,8 @@ second_left = TreeNode(2)
 second_right = TreeNode(2)
 root.left = second_left
 root.right = second_right
-second_left.left = TreeNode(3)
-second_left.right = TreeNode(4)
-second_right.left = TreeNode(4)
-second_right.right = TreeNode(3)
+second_left.left = TreeNode(2)
+second_right.left = TreeNode(2)
 
 solution = Solution()
 print(solution.isSymmetric(root = root))
