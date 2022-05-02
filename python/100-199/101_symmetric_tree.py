7dd59a809ceb09ddeb0211209cae1c69f291d5b4
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-02 16:59:06 +0300

    101_symmetric_tree

--- python/100-199/101_symmetric_tree.py
+++ python/100-199/101_symmetric_tree.py
@@ -2,8 +2,22 @@ from typing import Optional, List
 from treenode import TreeNode
 
 class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        response = self.postorderTraversal(root.left) if root and root.left else []
+        response += self.postorderTraversal(root.right) if root and root.right else []
+        response += [root.val] if root else []
+
+        return response
+
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return True
+        if not root:
+            return True
+        elif root.left and not root.right or root.right and not root.left:
+            return False
+        elif not root.left and not root.right:
+            return True
+
+        return self.postorderTraversal(root.left) == self.postorderTraversal(root.right)
 
 
 root = TreeNode(3)
