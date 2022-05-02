19d1add1c4603d4eaea911a7d078e6d8a452cfcd
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-02 14:38:11 +0300

    102_binary_tree_level_order_traversal

---
+++ python/100-199/102_binary_tree_level_order_traversal.py
@@ -0,0 +1,44 @@
+from typing import Optional, List
+from treenode import TreeNode
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        levels = {}
+
+        queue = [root]
+        level = 0
+
+        while len(queue) != 0:
+            level += 1
+            subarray = []
+            n = len(queue)
+            i = 0
+
+            while i < n:
+                element = queue.pop(0)
+
+                if element:
+                    subarray.append(element.val)
+
+                    if element.left:
+                        queue.append(element.left)
+
+                    if element.right:
+                        queue.append(element.right)
+
+                i += 1
+
+            if subarray:
+                levels[level] = subarray
+
+        return [list(val) for val in levels.values()]
+
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+solution = Solution()
+print(solution.levelOrder(root = root))
