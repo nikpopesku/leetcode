ae7054b4b1d9d759738e832d26ed7bad9322ca2e
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-05 18:02:00 +0300

    701_insert_into_a_binary_search_tree

--- python/600-699/653_two_sum_4_input_is_a_bst.py
+++ python/600-699/653_two_sum_4_input_is_a_bst.py
@@ -3,9 +3,29 @@ from treenode import TreeNode
 
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        return True
+        if not root:
+            return False
+
+        hash = {}
+        stack = [root]
+
+        while len(stack) != 0:
+            element = stack.pop(0)
+            if k - element.val in hash:
+                return True
+            else:
+                hash[element.val] = k - element.val
+
+            if element.left:
+                stack.append(element.left)
+
+            if element.right:
+                stack.append(element.right)
+
+
+        return False
 
 
 r1 = TreeNode(5, TreeNode(4), TreeNode(8, TreeNode(7, TreeNode(6)), TreeNode(9)))
 solution = Solution()
-print(solution.findTarget(root = r1))
+print(solution.findTarget(root = r1, k = 9))
