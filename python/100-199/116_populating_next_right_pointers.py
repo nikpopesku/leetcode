0c76f01e08c73580edea722ebed7c42efbcae50b
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-10 17:28:21 +0300

    116_populating_next_right_pointers

--- python/100-199/116_populating_next_right_pointers.py
+++ python/100-199/116_populating_next_right_pointers.py
@@ -3,7 +3,19 @@ from treenode import Node
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        return root
+        def inner_connect(node):
+            if node and node.left:
+                node.left.next = node.right
+                inner_connect(node.left)
+
+            if node and node.right:
+                if node.next:
+                    node.right.next = node.next.left
+                inner_connect(node.right)
+
+            return node
+
+        return inner_connect(root)
 
 
 r1 = Node(1, None, Node(2, Node(3)))
