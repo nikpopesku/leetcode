2bcdf4d9e14add1b44b9e068799aa4c0e33b8bfc
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-22 16:50:20 +0300

    77_combinations

--- python/0-99/77_combinations.py
+++ python/0-99/77_combinations.py
@@ -3,11 +3,22 @@ from typing import List
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return [[]]
+        numbers = list(range(1, n+1))
+
+        def xxx(numbers: List, response: List, k: int):
+            while numbers and k > 0:
+                response.append(numbers.pop())
+                k -= 1
+
+                return xxx(numbers, response, k)
+
+            return response
+
+        return xxx(numbers, [], k)
 
 
 
 
 
 solution = Solution()
-print(solution.combine(matrix = [[1,2]], target = 2))
+print(solution.combine(n = 4, k = 2))
