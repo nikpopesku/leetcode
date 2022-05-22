54d9606383a165844b98a3280bf0bf9fa254528a
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-22 20:07:30 +0300

    46_permutations

--- python/0-99/46_permutations.py
+++ python/0-99/46_permutations.py
@@ -3,20 +3,19 @@ from typing import List, Set
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def permute_all(offset, nums):
-            if offset == len(nums) - 1:
+        def permute_all(i):
+            if i == len(nums) - 1:
                 result.append(nums.copy())
 
-            j = offset + 1
+            j = i + 1
             while j < len(nums):
-                nums[offset], nums[j] = nums[j], nums[offset]
-                permute_all(j, nums)
-                nums[j], nums[offset] = nums[offset], nums[j]
+                nums[i], nums[j] = nums[j], nums[i]
+                permute_all(j)
+                nums[j], nums[i] = nums[i], nums[j]
                 j += 1
 
-
         result = []
-        permute_all(0, [])
+        permute_all(0, nums)
         return result
 
 
