2722b5de690e81aef5929fcc73d599adfcb14c35
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-22 16:05:36 +0300

    77_combinations

---
+++ python/0-99/77_combinations.py
@@ -0,0 +1,13 @@
+import sys
+from typing import List
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return [[]]
+
+
+
+
+
+solution = Solution()
+print(solution.combine(matrix = [[1,2]], target = 2))
