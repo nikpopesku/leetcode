f71ace968a81e27f7069497f5b5ae9cbe959453c
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-22 21:02:46 +0300

    784_letter_case_permutations

--- python/700-799/784_letter_case_permutations.py
+++ python/700-799/784_letter_case_permutations.py
@@ -4,12 +4,14 @@ from typing import List, Set
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         def permute_all(i, current_string):
-            if i == len(s) - 1:
+            if i >= len(s) - 1 and current_string not in result:
                 result.append(current_string)
 
             for j in range(i, len(s)):
                 if current_string[j].lower() >= 'a' and current_string[j].lower() <= 'z':
-                    changed_string = (current_string[:j] if j > 0 else '') + current_string[j].upper() + (current_string[j+1:] if j+1 < len(s) else '')
+                    changed_string = (current_string[:j] if j > 0 else '') + \
+                                     (current_string[j].upper() if current_string[j].lower() == current_string[j] else current_string[j].lower()) + \
+                                     (current_string[j+1:] if j+1 < len(s) else '')
                     permute_all(i+1, changed_string)
                 permute_all(i+1, current_string)
 
