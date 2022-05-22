7c7418cbd0390dde2e66c95d9366cb1252cdf868
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-22 20:14:58 +0300

    784_letter_case_permutations

---
+++ python/700-799/784_letter_case_permutations.py
@@ -0,0 +1,13 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        return []
+
+
+
+
+
+solution = Solution()
+print(solution.letterCasePermutation(s = "a1b2"))
