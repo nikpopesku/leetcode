b4b7cfc457626c5795be270eeacbff2c8e0b8f98
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-22 16:50:48 +0300

    77_combinations

--- python/0-99/77_combinations.py
+++ python/0-99/77_combinations.py
@@ -5,16 +5,16 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         numbers = list(range(1, n+1))
 
-        def xxx(numbers: List, response: List, k: int):
+        def combine_recurrent(numbers: List, response: List, k: int):
             while numbers and k > 0:
                 response.append(numbers.pop())
                 k -= 1
 
-                return xxx(numbers, response, k)
+                return combine_recurrent(numbers, response, k)
 
             return response
 
-        return xxx(numbers, [], k)
+        return combine_recurrent(numbers, [], k)
 
 
 
