ef96a5d90a501bc66ea872261fd9260ecdfb0a7a
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-23 20:23:31 +0300

    70_climbing_stairs

---
+++ python/100-199/198_house_robber.py
@@ -0,0 +1,12 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return 4
+
+
+
+
+solution = Solution()
+print(solution.rob(nums = [1,2,3,1]))
