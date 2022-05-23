aa57830e28189f59f65967ff76d2b3a6486ab2a9
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-23 17:01:36 +0300

    784_letter_case_permutations

--- python/700-799/784_letter_case_permutations.py
+++ python/700-799/784_letter_case_permutations.py
@@ -3,20 +3,21 @@ from typing import List, Set
 
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        def permute_all(i, current_string):
+        def permute_all(i, current_string: List[str]):
             if i == len(s):
                 result.add(current_string)
 
             for j in range(i, len(s)):
-                if current_string[j].lower() >= 'a' and current_string[j].lower() <= 'z':
+                lower = current_string[j].lower()
+                if 'a' <= lower <= 'z':
                     changed_string = (current_string[:j] if j > 0 else '') + \
-                                     (current_string[j].upper() if current_string[j].lower() == current_string[j] else current_string[j].lower()) + \
+                                     (current_string[j].upper() if lower == current_string[j] else lower) + \
                                      (current_string[j+1:] if j+1 < len(s) else '')
                     permute_all(i+1, changed_string)
                 permute_all(i+1, current_string)
 
         result = set()
-        permute_all(0, s)
+        permute_all(0, list(s))
         return list(result)
 
 
@@ -24,4 +25,4 @@ class Solution:
 
 
 solution = Solution()
-print(len(solution.letterCasePermutation(s = "qMKfHUf")))
+print(len(solution.letterCasePermutation(s = "k5qo0LdW")))
