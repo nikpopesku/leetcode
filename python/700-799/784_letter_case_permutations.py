3264cfe88ae3ff1fdb8acdbfae2adab67bf883de
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-23 17:13:00 +0300

    784_letter_case_permutations

--- python/700-799/784_letter_case_permutations.py
+++ python/700-799/784_letter_case_permutations.py
@@ -5,18 +5,16 @@ class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         def permute_all(i, current_string: List[str]):
             if i == len_s:
-                result.add(current_string)
+                result.append(current_string)
 
             for j in range(i, len_s):
                 lower = current_string[j].lower()
-                if 'a' <= lower <= 'z':
-                    changed_string = (current_string[:j] if j > 0 else '') + \
-                                     (current_string[j].upper() if lower == current_string[j] else lower) + \
-                                     (current_string[j+1:] if j+1 < len_s else '')
-                    permute_all(i+1, changed_string)
                 permute_all(i+1, current_string)
+                if 'a' <= lower <= 'z':
+                    current_string[j] = current_string[j].upper() if lower == current_string[j] else lower
+                    permute_all(i+1, current_string)
 
-        result = set()
+        result = []
         len_s = len(s)
         permute_all(0, list(s))
         return list(result)
