d73942db20e12c6c7398c572145d9012e0db932c
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-25 21:01:03 +0300

    120_triangle

--- python/100-199/120_triangle.py
+++ python/100-199/120_triangle.py
@@ -10,15 +10,17 @@ class Solution:
             return triangle[0][0]
 
         if length == 2:
-            return triangle[1][0] if triangle[1][0] > triangle[1][1] else triangle[1][1]
+            return triangle[1][1] if triangle[1][0] > triangle[1][1] else triangle[1][0]
 
-        houses[1] = nums[0]
-        houses[2] = nums[0] if nums[0] > nums[1] else nums[1]
+        nodes[0] = triangle[0][0]
+        nodes[1] = triangle[1][1] if triangle[1][0] > triangle[1][1] else triangle[1][0]
+        last_element = 1 if triangle[1][0] > triangle[1][1] else 0
 
-        for i in range(3, length+1):
-            houses[i] = max(houses[i-2] + nums[i-1], houses[i-1])
+        for i in range(2, length):
+            nodes[i] = min(triangle[i][last_element], triangle[i][last_element+1])
+            last_element = last_element if triangle[i][last_element] < triangle[i][last_element+1] else last_element + 1
 
-        return houses[length]
+        return sum(nodes.values())
 
 
 
