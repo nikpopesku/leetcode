28db8fc90607204202dc9509c96cc3c6043b4bb9
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-25 20:42:15 +0300

    120_triangle

--- python/100-199/120_triangle.py
+++ python/100-199/120_triangle.py
@@ -3,7 +3,22 @@ from typing import List, Set
 
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        return 11
+        nodes = {}
+        length = len(triangle)
+
+        if length == 1:
+            return triangle[0][0]
+
+        if length == 2:
+            return triangle[1][0] if triangle[1][0] > triangle[1][1] else triangle[1][1]
+
+        houses[1] = nums[0]
+        houses[2] = nums[0] if nums[0] > nums[1] else nums[1]
+
+        for i in range(3, length+1):
+            houses[i] = max(houses[i-2] + nums[i-1], houses[i-1])
+
+        return houses[length]
 
 
 
