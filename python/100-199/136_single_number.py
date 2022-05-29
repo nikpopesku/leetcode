52fa5d699250a230011412a7deb87781747ba1b7
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-29 12:50:27 +0300

    136_single_number

--- python/100-199/136_single_number.py
+++ python/100-199/136_single_number.py
@@ -1,10 +1,11 @@
-import sys
-from typing import List, Set
+from functools import reduce
+from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return 5
+        return reduce(lambda a, b: a ^ b, nums)
 
 
 solution = Solution()
-print(bin(solution.singleNumber(nums = [2,2,1])))
+print(bin(solution.    def singleNumber(self, nums: List[int]) -> int:
+(nums = [2,2,1])))
