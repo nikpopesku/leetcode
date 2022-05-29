795ac939319f855a251924cd99cd56f1da0e9b47
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-29 17:41:38 +0300

    1281_substract_product_and_sum_of_digits_of_integer

--- python/1200-1299/1281_count_odd_numbers_in_interval_range.py.py
+++ python/1200-1299/1281_count_odd_numbers_in_interval_range.py.py
@@ -2,15 +2,7 @@ from typing import List
 
 
 class Solution:
-    def average(self, salary: List[int]) -> float:
-        minimum, maximum, sum = salary[0], salary[0], salary[0]
-
-        for i in range(1, len(salary)):
-            minimum = min(minimum, salary[i])
-            maximum = max(maximum, salary[i])
-            sum += salary[i]
-
-        return round(((sum - minimum - maximum) / (len(salary) - 2)), 5)
+    def subtractProductAndSum(self, n: int) -> int:
 
 solution = Solution()
-print(solution.average(salary = [4000,3000,1000,2000]))
+print(solution.subtractProductAndSum(n = 234))
