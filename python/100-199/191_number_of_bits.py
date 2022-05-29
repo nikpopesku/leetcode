da07fdc6136b2cbf18f3f647cfef01d57ba8a32c
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-29 10:35:14 +0300

    191_number_of_1bits

---
+++ python/100-199/191_number_of_bits.py
@@ -0,0 +1,12 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        return 5
+
+
+
+
+solution = Solution()
+print(solution.hammingWeight(n = decimal('0b00000000000000000000000000001011')))
