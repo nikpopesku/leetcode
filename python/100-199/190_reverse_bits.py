3905388cf67208fba8de0c81341928e060f6c634
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-29 12:27:03 +0300

    190_reverse_bits

--- python/100-199/190_reverse_bits.py
+++ python/100-199/190_reverse_bits.py
@@ -4,7 +4,7 @@ from typing import List, Set
 class Solution:
     def reverseBits(self, n: int) -> int:
         response = 0
-        i = 32
+        i = 31
         while i:
             i -= 1
             response = (response + (n & 1)) << 1
