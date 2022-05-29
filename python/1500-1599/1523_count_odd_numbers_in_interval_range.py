039d9c246cbaf262db88afd49266525dd6ddac81
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-29 16:04:15 +0300

    1523_count_odd_numbers_in_interval_range

--- python/1500-1599/1523_count_odd_numbers_in_interval_range.py
+++ python/1500-1599/1523_count_odd_numbers_in_interval_range.py
@@ -3,6 +3,7 @@ from typing import List
 
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
+        return (high - low) // 2 + (1 if low % 2 == 1 or high % 2 == 1 else 0)
 
 
 solution = Solution()
