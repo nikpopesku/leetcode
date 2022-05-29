c975a5f6744603c5dcfb7aee9fc38b12a89a1812
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-29 10:59:35 +0300

    190_reverse_bits

---
+++ python/100-199/190_reverse_bits.py
@@ -0,0 +1,10 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        return 8
+
+
+solution = Solution()
+print(solution.reverseBits(n = 5))
