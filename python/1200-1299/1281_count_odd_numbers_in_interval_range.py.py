b0a41a3d967261c0386490c4cafddcb1395a85b1
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-29 17:50:05 +0300

    1281_substract_product_and_sum_of_digits_of_integer

--- python/1200-1299/1281_count_odd_numbers_in_interval_range.py.py
+++ python/1200-1299/1281_count_odd_numbers_in_interval_range.py.py
@@ -3,6 +3,14 @@ from typing import List
 
 class Solution:
     def subtractProductAndSum(self, n: int) -> int:
+        sum, product = 0, 1
+        while n:
+            digit = n % 10
+            n = n // 10
+            product *= digit
+            sum += digit
+
+        return product - sum
 
 solution = Solution()
 print(solution.subtractProductAndSum(n = 234))
