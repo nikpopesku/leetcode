827257498a12f7510b996d7b93d52113a7e9733a
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-29 12:22:50 +0300

    190_reverse_bits

--- python/100-199/190_reverse_bits.py
+++ python/100-199/190_reverse_bits.py
@@ -5,9 +5,8 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         response = 0
         while n:
-            temp = n & 1
+            response = (response + n & 1) << 1
             n = n >> 1
-            response = (response + temp) << 1
 
         return response
 
