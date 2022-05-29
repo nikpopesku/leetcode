51b45e5d28188de43367bd6c4741ac0e98c92e80
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-29 12:34:11 +0300

    136_single_number

---
+++ python/100-199/136_single_number.py
@@ -0,0 +1,10 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return 5
+
+
+solution = Solution()
+print(bin(solution.singleNumber(nums = [2,2,1])))
