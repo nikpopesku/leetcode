773f00d8bd3a8c132084ba9e6abbac8220807f76
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-29 17:31:03 +0300

    1491_average_salary_excluding_minimum_and_maximum_salary

--- python/1500-1599/1523_count_odd_numbers_in_interval_range.py
+++ python/1500-1599/1523_count_odd_numbers_in_interval_range.py
@@ -2,9 +2,15 @@ from typing import List
 
 
 class Solution:
-    def countOdds(self, low: int, high: int) -> int:
-        return (high - low) // 2 + (1 if low % 2 == 1 or high % 2 == 1 else 0)
+    def average(self, salary: List[int]) -> float:
+        minimum, maximum, sum = salary[0], salary[0], salary[0]
 
+        for i in range(1, len(salary)):
+            minimum = min(minimum, salary[i])
+            maximum = max(maximum, salary[i])
+            sum += salary[i]
+
+        return round(((sum - minimum - maximum) / (len(salary) - 2)), 5)
 
 solution = Solution()
-print(solution.countOdds(low = 3, high = 7))
+print(solution.average(salary = [4000,3000,1000,2000]))
