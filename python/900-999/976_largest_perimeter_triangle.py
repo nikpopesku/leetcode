ff763a888ad1bf84a1b62fdb7442d20d0afdf8e3
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-30 20:33:36 +0300

    976_largest_perimeter_triangle

---
+++ python/900-999/976_largest_perimeter_triangle.py
@@ -0,0 +1,10 @@
+from typing import List
+from functools import reduce
+
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        return 5
+
+solution = Solution()
+print(solution.largestPerimeter(nums = [2,1,2]))
