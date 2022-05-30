efc185c8e0b2fea1477dbef6f8891fe20e4e4118
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-30 20:56:59 +0300

    976_largest_perimeter_triangle

--- python/900-999/976_largest_perimeter_triangle.py
+++ python/900-999/976_largest_perimeter_triangle.py
@@ -4,7 +4,15 @@ from functools import reduce
 
 class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
-        return 5
+        nums.sort()
+        counter = len(nums) - 1
+        candidates = [nums[counter - 2], nums[counter - 1], nums[counter]]
+
+        while not rule:
+            candidates.sort()
+            candidates.pop()
+            nums.pop()
+
 
 solution = Solution()
 print(solution.largestPerimeter(nums = [2,1,2]))
