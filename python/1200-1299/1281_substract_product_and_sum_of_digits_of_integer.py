5a93251a87adb3a9300cff2731f64218e3a278b0
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-30 20:30:48 +0300

    UN-19082: Fixed branch dashboard

--- python/1200-1299/1281_substract_product_and_sum_of_digits_of_integer.py
+++ python/1200-1299/1281_substract_product_and_sum_of_digits_of_integer.py
@@ -1,16 +1,10 @@
 from typing import List
-
+from functools import reduce
 
 class Solution:
     def subtractProductAndSum(self, n: int) -> int:
-        sum, product = 0, 1
         l = list(str(n))
-        while l:
-            digit = int(l.pop())
-            product *= digit
-            sum += digit
-
-        return product - sum
+        return reduce(lambda x, y: int(x) * int(y)  , l) - reduce(lambda x, y: int(x) + int(y)  , l)
 
 solution = Solution()
 print(solution.subtractProductAndSum(n = 234))
