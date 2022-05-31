09af3059065870bcfaaab016bdab41be5f0fccc5
npopesku <nikolay.popesku@unbiased.co.uk>
2022-05-31 10:35:11 +0300

    1779_find_nearest_point_that_has_same_coordinate

--- python/1700-1799/1779_find_nearest_point_that_has_same_coordinate.py
+++ python/1700-1799/1779_find_nearest_point_that_has_same_coordinate.py
@@ -7,8 +7,8 @@ class Solution:
         minimum = float('inf')
         minimum_position = -1
         for i in range(len(points)):
-            if (points[i][0] == x or points[i][1] == y) and (abs(x - points[i][0]) + abs(x - points[i][0]) < minimum):
-                minimum = abs(x - points[i][0]) + abs(x - points[i][0])
+            if (points[i][0] == x or points[i][1] == y) and (abs(x - points[i][0]) + abs(y - points[i][1]) < minimum):
+                minimum = abs(x - points[i][0]) + abs(y - points[i][1])
                 minimum_position = i
 
         return minimum_position
