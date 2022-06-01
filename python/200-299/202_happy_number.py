a692daa1f074fca78cc1f70d4447053d6cc8d097
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-01 11:31:26 +0300

    202_happy_number

--- python/200-299/202_happy_number.py
+++ python/200-299/202_happy_number.py
@@ -4,6 +4,17 @@ from functools import reduce
 
 class Solution:
     def isHappy(self, n: int) -> bool:
+        total_sum = 0
+        while n // 10 != 0:
+            while n:
+                digit = n % 10
+                n = n // 10
+                total_sum += digit ** 2
+            if total_sum == 1:
+                return True
+            n = total_sum
+
+        return True if n == 1 else False
 
 solution = Solution()
 print(solution.isHappy(n = 19))
