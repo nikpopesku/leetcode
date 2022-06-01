a9372484b51c105e1b1d1dc4ad249e46a452485c
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-01 11:19:22 +0300

    1502_can_make_arithmetic_progression_from_sequence

--- python/1500-1599/1502_can_make_arithmetic_progression_from_sequence.py
+++ python/1500-1599/1502_can_make_arithmetic_progression_from_sequence.py
@@ -4,6 +4,13 @@ from functools import reduce
 
 class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        delta = arr[1] - arr[0]
+        for i in range(2, len(arr)):
+            if arr[i] - delta != arr[i-1]:
+                return False
+
+        return True
 
 
 solution = Solution()
