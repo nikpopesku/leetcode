cb6c2edf1ed3849b8f198d8a577845f5a7a391c7
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-01 11:14:53 +0300

    1502_can_make_arithmetic_progression_from_sequence

--- python/1500-1599/1502_can_make_arithmetic_progression_from_sequence.py
+++ python/1500-1599/1502_can_make_arithmetic_progression_from_sequence.py
@@ -3,16 +3,8 @@ from functools import reduce
 
 
 class Solution:
-    def arraySign(self, nums: List[int]) -> int:
-        product = 1
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                return 0
-            product *= 1 if nums[i] > 0 else -1
-
-        return product
-
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
 
 
 solution = Solution()
-print(solution.nearestValidPoint(x = 3, y = 4, points = [[1,2],[3,1],[2,4],[2,3],[4,4]]))
+print(solution.canMakeArithmeticProgression(arr = [3,5,1]))
