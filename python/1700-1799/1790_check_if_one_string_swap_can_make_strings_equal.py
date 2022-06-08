ae1e33f52ad532374a496a0f1e1316f59ec90762
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-08 09:45:36 +0300

    1790_check_if_one_string_swap_can_make_strings_equal

---
+++ python/1700-1799/1790_check_if_one_string_swap_can_make_strings_equal.py
@@ -0,0 +1,10 @@
+from typing import List
+from functools import reduce
+
+
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        return True
+
+solution = Solution()
+print(solution.areAlmostEqual(s1 = "bank", s2 = "kanb"))
