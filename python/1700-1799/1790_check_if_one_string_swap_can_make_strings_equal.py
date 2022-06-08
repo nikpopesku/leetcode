b8d3967fb7b91edac459ef788284b85fe9c02079
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-08 09:52:32 +0300

    1790_check_if_one_string_swap_can_make_strings_equal

--- python/1700-1799/1790_check_if_one_string_swap_can_make_strings_equal.py
+++ python/1700-1799/1790_check_if_one_string_swap_can_make_strings_equal.py
@@ -4,7 +4,19 @@ from functools import reduce
 
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        return True
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                break
+        else:
+            return True
+
+        for j in range(i+1, len(s1)):
+            if s1[j] != s2[j]:
+                break
+        else:
+            return False
+
+        return s1[:i] + s1[j] + s1[i+1:j] + s1[i] + s1[j+1:] == s2
 
 solution = Solution()
 print(solution.areAlmostEqual(s1 = "bank", s2 = "kanb"))
