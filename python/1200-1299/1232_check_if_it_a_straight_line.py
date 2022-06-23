2548b1d0fe79c7774b3301a9b86ad937d02ec516
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-23 15:55:02 +0300

    1232_check_if_it_a_straight_line

--- python/1200-1299/1232_check_if_it_a_straight_line.py
+++ python/1200-1299/1232_check_if_it_a_straight_line.py
@@ -1,10 +1,18 @@
 from typing import List
-
+from math import atan
 
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        proportion0 = (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0])
+        for i in range(2, len(coordinates)):
+            if proportion0 != (coordinates[i][1] - coordinates[1][1]) / (coordinates[i][0] - coordinates[1][0]):
+                return False
+        else:
+            return True
+
+
 
 
 
 solution = Solution()
-print(solution.nextGreaterElement(nums1 = [2,4], nums2 = [1,2,3,4]))
+print(solution.checkStraightLine(coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]))
