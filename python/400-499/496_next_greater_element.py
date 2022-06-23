31f3a1822f623768863930105055c8c8ce078104
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-23 15:34:17 +0300

    496_next_greater_element_I

--- python/400-499/496_next_greater_element.py
+++ python/400-499/496_next_greater_element.py
@@ -1,9 +1,25 @@
-import typing import List
+from typing import List
 
 
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return []
+        hash = {}
+        for index, value in enumerate(nums2):
+            hash[value] = index
+
+        response = []
+
+        for index, value in enumerate(nums1):
+            place = hash[value]
+            answer = -1
+
+            for i in range(nums2[place+1:]):
+                if nums2[i] > value:
+                    answer = nums2[i]
+                    break
+            response.append(answer)
+
+        return response
 
 
 
