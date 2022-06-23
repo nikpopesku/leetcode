19912534d7b1cdf15cb9f92ce72a7120f8a7e1dc
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-23 15:43:39 +0300

    1232_check_if_it_a_straight_line

---
+++ python/1200-1299/1232_check_if_it_a_straight_line.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+
+
+
+solution = Solution()
+print(solution.nextGreaterElement(nums1 = [2,4], nums2 = [1,2,3,4]))
