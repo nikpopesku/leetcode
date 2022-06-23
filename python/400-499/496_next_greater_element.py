60fefd91723fd5b10721e0ac566ed6cda35766c9
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-23 15:18:40 +0300

    589_n_ary_tree_preorder-traversal

--- python/400-499/496_next_greater_element.py
+++ python/400-499/496_next_greater_element.py
@@ -2,19 +2,7 @@ import sys
 
 
 class Solution:
-    def longestPalindrome(self, s: str) -> int:
-        right = []
-        without_pair = {}
-        for key in s:
-            if key not in without_pair:
-                without_pair[key] = key
-            else:
-                right.append(key)
-                del without_pair[key]
-
-
-        return (1 if len(without_pair) >= 1 else 0) + 2 * len(right)
-
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
 
 
 
