d17e9a7d6c4b5803901fed24e0026cb7211c3e53
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-23 15:59:35 +0300

    1232_check_if_it_a_straight_line

--- python/1200-1299/1232_check_if_it_a_straight_line.py
+++ python/1200-1299/1232_check_if_it_a_straight_line.py
@@ -3,9 +3,10 @@ from math import atan
 
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        proportion0 = (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0])
+        proportion0 = (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]) if (coordinates[1][0] - coordinates[0][0]) != 0 else 'x'
         for i in range(2, len(coordinates)):
-            if proportion0 != (coordinates[i][1] - coordinates[1][1]) / (coordinates[i][0] - coordinates[1][0]):
+            proportion =  (coordinates[i][1] - coordinates[1][1]) / (coordinates[i][0] - coordinates[1][0]) if (coordinates[i][0] - coordinates[1][0]) != 0 else 'x'
+            if proportion0 != proportion:
                 return False
         else:
             return True
