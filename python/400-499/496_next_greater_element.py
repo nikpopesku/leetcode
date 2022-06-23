217b16166ed5bbcc01d6b084feeb244d2916257f
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-23 15:21:08 +0300

    496_next_greater_element_I

--- python/400-499/496_next_greater_element.py
+++ python/400-499/496_next_greater_element.py
@@ -1,10 +1,11 @@
-import sys
+import typing import List
 
 
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return []
 
 
 
 solution = Solution()
-print(solution.longestPalindrome(sys.argv[1]))
+print(solution.nextGreaterElement(nums1 = [4,1,2], nums2 = [1,3,4,2]))
