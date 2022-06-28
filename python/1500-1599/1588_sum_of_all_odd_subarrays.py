fcdf09a7e62c0b29157b4179132234f5f4f1bf13
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-28 20:30:54 +0300

    1588_sum_of_all_odd_subarrays

---
+++ python/1500-1599/1588_sum_of_all_odd_subarrays.py
@@ -0,0 +1,10 @@
+from typing import List
+from ntreenode import Node
+
+
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+
+
+solution = Solution()
+print(solution.sumOddLengthSubarrays(arr = [1,4,2,5,3]))
