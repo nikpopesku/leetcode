0c7917e943889db6da5e242abf9690a870a93256
npopesku <nikolay.popesku@unbiased.co.uk>
2022-06-28 22:30:39 +0300

    1588_sum_of_all_odd_subarrays

--- python/1500-1599/1588_sum_of_all_odd_subarrays.py
+++ python/1500-1599/1588_sum_of_all_odd_subarrays.py
@@ -10,16 +10,18 @@ class Solution:
         elif len(arr) == 4:
             return sum(arr) + sum(arr[:3]) + sum(arr[1:4])
 
-        numb = (len(arr) // 2) + 1 if len(arr) / 2 > len(arr) // 2 else len(arr) // 2
+        numb = len(arr) // 2 if len(arr) / 2 > len(arr) // 2 else (len(arr) // 2) - 1
 
         sum_all = 0
         for i in range(len(arr)):
+            if len(arr) % 2 == 1:
+                numb = numb + 1 if i <= len(arr) / 2 else numb - 1
+            elif not ((i == len(arr) // 2) and len(arr) % 2 == 0):
+                numb = numb + 1 if (i + 1) <= len(arr) / 2 else numb - 1
             sum_all += arr[i] * numb
-            if i - 1 == len(arr) // 2 and len(arr) % 2 == 0:
-                continue
-            numb = numb + 1 if (i + 1) <= len(arr) / 2 else numb - 1
+
 
         return sum_all
 
 solution = Solution()
-print(solution.sumOddLengthSubarrays(arr = [1,1,1,1,1,1,1,1,1,1]))
+print(solution.sumOddLengthSubarrays(arr = [1,4,2,5,3]))
