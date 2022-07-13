e0eebdf134d45ba32f1c0481e02098d90be66cdd
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-13 11:04:47 +0300

    1672_richest_customer_wealth

---
+++ python/1600-1699/1672_richest_cutomer_wealth.py
@@ -0,0 +1,10 @@
+from typing import List
+from functools import reduce
+
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return 5
+
+solution = Solution()
+print(solution.maximumWealth(accounts = [[1,2,3],[3,2,1]]))
