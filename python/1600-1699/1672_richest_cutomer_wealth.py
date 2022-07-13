a918939483872d838feacce040e279c84694424c
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-13 11:19:03 +0300

    1672_richest_customer_wealth

--- python/1600-1699/1672_richest_cutomer_wealth.py
+++ python/1600-1699/1672_richest_cutomer_wealth.py
@@ -3,8 +3,8 @@ from functools import reduce
 
 
 class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        return 5
+    def maximumWealth(self, accounts: List[List[int]]) -> list:
+        return max([sum(i) for i in accounts])
 
 solution = Solution()
 print(solution.maximumWealth(accounts = [[1,2,3],[3,2,1]]))
