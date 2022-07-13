c1232a9f11c98de5e1cc9fcde01f81e3eaf82808
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-13 11:33:23 +0300

    1768_merge_string_alternately

---
+++ python/1700-1799/1768_merge_string_alternately.py
@@ -0,0 +1,12 @@
+from typing import List
+from functools import reduce
+
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        return ''
+
+
+
+solution = Solution()
+print(solution.mergeAlternately(word1 = "abc", word2 = "pqr"))
