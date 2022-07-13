cd0cc9030871e14855618677a67e24882bf9ce99
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-13 11:24:49 +0300

    1572_matrix_diagonal_sum

--- python/1500-1599/1572_matrix_diagonal_sum.py
+++ python/1500-1599/1572_matrix_diagonal_sum.py
@@ -4,6 +4,7 @@ from functools import reduce
 
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
+        return sum([mat[i][i] for i in range(len(mat))])
 
 solution = Solution()
 print(solution.diagonalSum(mat = [[1,2,3],
