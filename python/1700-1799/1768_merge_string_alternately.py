eaf7a787889bde19ce0a80f531aa2e7e79563cb7
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-13 11:42:20 +0300

    1768_merge_string_alternately

--- python/1700-1799/1768_merge_string_alternately.py
+++ python/1700-1799/1768_merge_string_alternately.py
@@ -4,7 +4,7 @@ from functools import reduce
 
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        return ''
+        return ''.join([word1[i] + word2[i] for i in range(min(len(word1), len(word2)))]) + (word1[min(len(word1), len(word2))] if len(word1) > len(word2) else word2[min(len(word1), len(word2)):])
 
 
 
