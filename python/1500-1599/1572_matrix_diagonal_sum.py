df65fe3309a8f415e525b030a735ddcc1f11449d
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-13 11:23:15 +0300

    1572_matrix_diagonal_sum

---
+++ python/1500-1599/1572_matrix_diagonal_sum.py
@@ -0,0 +1,11 @@
+from typing import List
+from functools import reduce
+
+
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+
+solution = Solution()
+print(solution.diagonalSum(mat = [[1,2,3],
+              [4,5,6],
+              [7,8,9]]))
