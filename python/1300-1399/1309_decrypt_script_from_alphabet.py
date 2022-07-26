446433301ff9cfa85d4ce3efc3d4b89d4b247035
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-26 22:01:32 +0300

    1309_decrypt_script_from_alphabet

--- python/1300-1399/1309_decrypt_script_from_alphabet.py
+++ python/1300-1399/1309_decrypt_script_from_alphabet.py
@@ -4,14 +4,17 @@ from functools import reduce
 
 class Solution:
     def freqAlphabets(self, s: str) -> str:
-        hashmap = {'()': 'o', '(al)': 'al'}
-        lengths = [len(i) for i in hashmap.values()]
+        hashmap = {'1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f', '7': 'g', '8': 'h', '9': 'i'}
+        for i in range(10, 27):
+            hashmap[str(i) + '#'] = chr(ord('j') + i - 10)
+
+        lengths = set([len(i) for i in hashmap.values()])
         answer = []
         i = 0
         while i < len(s):
             for length in lengths:
                 if (i + length) <= len(s) and s[i:i+length] in hashmap.keys():
-                    element = hashmap[s[i:i+length]]['value']
+                    element = hashmap[s[i:i+length]]
                     break
             else:
                 length = 1
