4893ab7174389ac898a1db123871a4c81af5cdd1
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-26 21:35:03 +0300

    389_find_the_difference

--- python/300-399/389_find_the_difference.py
+++ python/300-399/389_find_the_difference.py
@@ -4,7 +4,14 @@ from functools import reduce
 
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        return ''.join(set(t) - set(s))
+        hash = {}
+        for i in range(len(s)):
+            hash[s[i]] = hash[s[i]] + 1 if s[i] in hash.keys() else 1
+        for i in range(len(t)):
+            if s[i] not in hash.keys() or hash[s[i]] == 0:
+                return s[i]
+            else:
+                hash[s[i]] = hash[s[i]] - 1
 
 
 solution = Solution()
