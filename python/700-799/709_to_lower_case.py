04a073ee21da23fe953da8446e8023309328d4e3
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-26 21:47:28 +0300

    709_to_lower_case

--- python/700-799/709_to_lower_case.py
+++ python/700-799/709_to_lower_case.py
@@ -7,7 +7,7 @@ class Solution:
         answer = []
         for i in range(len(s)):
             delta = 32 if 65 <= ord(s[i]) <= 90 else 0
-            answer.append(ord(s[i]) + delta)
+            answer.append(chr(ord(s[i]) + delta))
 
         return ''.join(answer)
 
