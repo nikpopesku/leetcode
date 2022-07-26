97d42e52c21acee1faf766686a6aa5bc05c54582
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-26 19:07:38 +0300

    fix

--- python/0-99/77_combinations.py
+++ python/0-99/77_combinations.py
@@ -3,27 +3,22 @@ from typing import List, Set
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        numbers = list(range(1, n+1))
-        response = []
+        def directed_combinations(offset, partial_combination):
+            if len(partial_combination) == k:
+                result.append(list(partial_combination))
+                return
 
-        def combine_recurrent(numbers: List, response: List, temp_solution: Set, places: int):
-            while numbers and places > 0:
-                temp_solution.add(numbers.pop())
-                places -= 1
+            # Generate remaining combinations over {offset, ..., n - 1} of size
+            # num_remaining.
+            num_remaining = k - len(partial_combination)
+            i = offset
+            while i <= n and num_remaining <= n - i + 1:
+                directed_combinations(i + 1, partial_combination + [i])
+                i += 1
 
-                return combine_recurrent(numbers, response, temp_solution, places)
-
-            if temp_solution not in response:
-                response.append(temp_solution.copy())
-
-            if numbers:
-                temp_solution.pop()
-                places += 1
-                return combine_recurrent(numbers, response, temp_solution, places)
-
-            return response
-
-        return [list(i) for i in combine_recurrent(numbers, response, set(), k)]
+        result = []
+        directed_combinations(1, [])
+        return result
 
 
 
