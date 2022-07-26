cb46dabbdb8643a364341c6113ecdcd467531d4a
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-26 21:18:38 +0300

    1678_goal_parser_interpretation

--- python/1600-1699/1678_goal_parser_interpretation.py
+++ python/1600-1699/1678_goal_parser_interpretation.py
@@ -4,6 +4,22 @@ from functools import reduce
 
 class Solution:
     def interpret(self, command: str) -> str:
+        hashmap = {'()': {'length': 2, 'value': 'o'}, '(al)': {'length': 4, 'value': 'al'}}
+        lengths = [i['length'] for i in hashmap.values()]
+        answer = []
+        i = 0
+        while i < len(command):
+            for length in lengths:
+                if (i + length) < len(command) and command[i:i+length] in hashmap.keys():
+                    element = hashmap[command[i:i+length]]['value']
+                    break
+            else:
+                length = 1
+                element = command[i]
+            answer.append(element)
+            i = i + length
+
+        return str(answer)
 
 solution = Solution()
-print(solution.interpret(accounts = [[1,2,3],[3,2,1]]))
+print(solution.interpret(command = "G()(al)"))
