1f9be9a3f6d01e0aeec7d694873224e05f3066cd
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-26 21:56:54 +0300

    1309_decrypt_script_from_alphabet

--- python/1300-1399/1309_decrypt_script_from_alphabet.py
+++ python/1300-1399/1309_decrypt_script_from_alphabet.py
@@ -4,7 +4,22 @@ from functools import reduce
 
 class Solution:
     def freqAlphabets(self, s: str) -> str:
+        hashmap = {'()': 'o', '(al)': 'al'}
+        lengths = [len(i) for i in hashmap.values()]
+        answer = []
+        i = 0
+        while i < len(s):
+            for length in lengths:
+                if (i + length) <= len(s) and s[i:i+length] in hashmap.keys():
+                    element = hashmap[s[i:i+length]]['value']
+                    break
+            else:
+                length = 1
+                element = s[i]
+            answer.append(element)
+            i = i + length
 
+        return ''.join(answer)
 
 solution = Solution()
 print(solution.freqAlphabets(s = "10#11#12"))
