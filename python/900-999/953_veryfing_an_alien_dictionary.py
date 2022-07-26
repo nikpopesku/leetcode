efae7307d2c3a68513a207ca89af5999f7c0eaf1
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-26 22:13:38 +0300

    1309_decrypt_script_from_alphabet

---
+++ python/900-999/953_veryfing_an_alien_dictionary.py
@@ -0,0 +1,9 @@
+from typing import List
+from functools import reduce
+
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+
+solution = Solution()
+print(solution.isAlienSorted(words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"))
