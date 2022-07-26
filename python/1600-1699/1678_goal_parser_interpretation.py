371706d6af3db3bbad038e35a967e065d5caf411
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-26 21:03:30 +0300

    1678_goal_parser_interpretation

---
+++ python/1600-1699/1678_goal_parser_interpretation.py
@@ -0,0 +1,9 @@
+from typing import List
+from functools import reduce
+
+
+class Solution:
+    def interpret(self, command: str) -> str:
+
+solution = Solution()
+print(solution.interpret(accounts = [[1,2,3],[3,2,1]]))
