fccd338f07181ab78b6dd35ba141f36e04185988
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-26 22:30:18 +0300

    1309_decrypt_script_from_alphabet

--- python/900-999/953_veryfing_an_alien_dictionary.py
+++ python/900-999/953_veryfing_an_alien_dictionary.py
@@ -11,18 +11,18 @@ class Solution:
             hashmap[i] = key
 
         for i in range(1, len(words)):
-            if len(words[i-1]) > len(words[i]):
-                return False
-
             size = len(words[i-1])
             for j in range(size):
                 if hashmap[words[i-1][j]] > hashmap[words[i][j]]:
                     return False
                 elif hashmap[words[i-1][j]] < hashmap[words[i][j]]:
                     break
+            else:
+                if len(words[i-1]) > len(words[i]):
+                    return False
 
         return True
 
 
 solution = Solution()
-print(solution.isAlienSorted(words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"))
+print(solution.isAlienSorted(words = ["kuvp","q"], order = "ngxlkthsjuoqcpavbfdermiywz"))
