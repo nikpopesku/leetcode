8fac46b9099ff788e63e80a64ed3eef9d6827219
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-26 22:28:04 +0300

    1309_decrypt_script_from_alphabet

--- python/900-999/953_veryfing_an_alien_dictionary.py
+++ python/900-999/953_veryfing_an_alien_dictionary.py
@@ -4,6 +4,25 @@ from functools import reduce
 
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
+        hashmap = {}
+        key = 0
+        for i in order:
+            key = key + 1
+            hashmap[i] = key
+
+        for i in range(1, len(words)):
+            if len(words[i-1]) > len(words[i]):
+                return False
+
+            size = len(words[i-1])
+            for j in range(size):
+                if hashmap[words[i-1][j]] > hashmap[words[i][j]]:
+                    return False
+                elif hashmap[words[i-1][j]] < hashmap[words[i][j]]:
+                    break
+
+        return True
+
 
 solution = Solution()
 print(solution.isAlienSorted(words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"))
