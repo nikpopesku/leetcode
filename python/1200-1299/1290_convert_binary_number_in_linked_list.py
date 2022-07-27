bb15e710ddaa0a9abb79094a3ce675b824fca449
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-27 17:29:35 +0300

    1290_conver_binary_number_in_linked_list

--- python/1200-1299/1290_convert_binary_number_in_linked_list.py
+++ python/1200-1299/1290_convert_binary_number_in_linked_list.py
@@ -4,8 +4,13 @@ from listnode import ListNode
 
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
-        return 5
+        response = 0
+        while head:
+            response = (response << 1) | head.val
+            head = head.next
+
+        return response
 
 
 solution = Solution()
-print(solution.getDecimalValue(head = [1,0,1]))
+print(solution.getDecimalValue(head = ListNode(1, ListNode(0, ListNode(1)))))
