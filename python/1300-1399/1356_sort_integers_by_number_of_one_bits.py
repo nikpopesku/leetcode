b619f3c4ae00ed931dd96419c68a29b8a262ed2b
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-27 18:04:40 +0300

    1356_sort_integers_by_number_of_one_bits

--- python/1300-1399/1356_sort_integers_by_number_of_one_bits.py
+++ python/1300-1399/1356_sort_integers_by_number_of_one_bits.py
@@ -2,7 +2,23 @@ from typing import List
 
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
-        return [0,1,2,4,8,3,5,6,7]
+        hashmap = {}
+
+        for i in range(len(arr)):
+            count_bits = 0
+            element = arr[i]
+            while element:
+                count_bits = count_bits + (element & 1)
+                element = element >> 1
+            hashmap[count_bits] = [arr[i]] if count_bits not in hashmap.keys() else hashmap[count_bits] + [arr[i]]
+        dict(sorted(hashmap.items()))
+
+        response = []
+        for v in hashmap.values():
+            v.sort()
+            response = response + v
+
+        return response
 
 solution = Solution()
 print(solution.sortByBits(arr = [0,1,2,3,4,5,6,7,8]))
