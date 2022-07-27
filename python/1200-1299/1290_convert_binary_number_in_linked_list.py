887401e8775b75e72178ebad5200ddc981f21c2c
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-27 17:23:53 +0300

    1290_conver_binary_number_in_linked_list

--- python/1200-1299/1290_convert_binary_number_in_linked_list.py
+++ python/1200-1299/1290_convert_binary_number_in_linked_list.py
@@ -1,28 +1,11 @@
 from typing import List
-from functools import reduce
+from listnode import ListNode
 
 
 class Solution:
-    def isAlienSorted(self, words: List[str], order: str) -> bool:
-        hashmap = {}
-        key = 0
-        for i in order:
-            key = key + 1
-            hashmap[i] = key
-
-        for i in range(1, len(words)):
-            size = min(len(words[i-1]), len(words[i]))
-            for j in range(size):
-                if hashmap[words[i-1][j]] > hashmap[words[i][j]]:
-                    return False
-                elif hashmap[words[i-1][j]] < hashmap[words[i][j]]:
-                    break
-            else:
-                if len(words[i-1]) > len(words[i]):
-                    return False
-
-        return True
+    def getDecimalValue(self, head: ListNode) -> int:
 
 
 solution = Solution()
-print(solution.isAlienSorted(words = ["kuvp","q"], order = "ngxlkthsjuoqcpavbfdermiywz"))
+print(solution.class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:(words = ["kuvp","q"], order = "ngxlkthsjuoqcpavbfdermiywz"))
