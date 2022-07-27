c28f83725f6d139fda60dd6a42a1c6bc48ef0426
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-27 17:38:56 +0300

    404_sum_of_left_leaves

---
+++ python/400-499/404_sum_of_left_leaves.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from treenode import TreeNode
+
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+
+
+root = TreeNode(0)
+
+solution = Solution()
+print(solution.sumOfLeftLeaves(root = root))
