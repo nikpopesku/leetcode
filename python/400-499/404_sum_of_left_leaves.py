56417993e2b5f3a6299c1ccf3e6174698d4dab14
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-27 17:44:41 +0300

    404_sum_of_left_leaves

--- python/400-499/404_sum_of_left_leaves.py
+++ python/400-499/404_sum_of_left_leaves.py
@@ -3,9 +3,18 @@ from treenode import TreeNode
 
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        queue = [root] if root else []
+        left_list = []
+        while queue:
+            element = queue.pop(0)
+            if element:
+                if element.left:
+                    queue.append(element.left)
+                    left_list.append(element.left.val)
+                if element.right:
+                    queue.append(element.right)
 
-
-root = TreeNode(0)
+        return sum(left_list)
 
 solution = Solution()
-print(solution.sumOfLeftLeaves(root = root))
+print(solution.sumOfLeftLeaves(root = TreeNode(3, TreeNode(9, None, None),TreeNode(20, TreeNode(15, None, None), TreeNode(7, None, None)))))
