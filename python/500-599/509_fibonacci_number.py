f6f571867f2c24e1ef4809c59f6883b19f8d2a59
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-28 18:30:58 +0300

    509_fibonacci_number

--- python/500-599/509_fibonacci_number.py
+++ python/500-599/509_fibonacci_number.py
@@ -3,8 +3,16 @@ from typing import List
 
 
 class Solution:
+    def __init__(self):
+        self.hashtable = {0: 0, 1: 1}
+
     def fib(self, n: int) -> int:
-        return 5
+        number = self.fib(n - 1) + self.fib(n - 2) if n not in self.hashtable else self.hashtable[n]
+
+        if n not in self.hashtable:
+            self.hashtable[n] = number
+
+        return number
 
 
 solution = Solution()
