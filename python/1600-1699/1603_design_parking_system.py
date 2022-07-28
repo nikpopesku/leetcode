7bd1c1f5e66d5252ceaba0e704971294eaaf28fa
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-28 15:20:27 +0300

    1603_design_parking_system

---
+++ python/1600-1699/1603_design_parking_system.py
@@ -0,0 +1,15 @@
+from typing import List
+from functools import reduce
+
+
+class ParkingSystem:
+    def __init__(self, big: int, medium: int, small: int):
+        pass
+
+    def addCar(self, carType: int) -> bool:
+        return True
+
+
+obj = ParkingSystem(1, 2, 3)
+param_1 = obj.addCar(2)
+# print(solution.maximumWealth(accounts = [[1,2,3],[3,2,1]]))
