9f7c1075820de9e965e8610c15fb62f746abfd07
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-28 20:10:53 +0300

    746_min_cost_climbing_stairs

--- python/1100-1199/1137_n_th_tribonacci_number.py
+++ python/1100-1199/1137_n_th_tribonacci_number.py
@@ -6,8 +6,8 @@ class Solution:
         self.hashmap = {0: 0, 1: 1, 2: 1}
 
     def tribonacci(self, n: int) -> int:
-        if n not in self.hashmap:
-            self.hashmap[n] = self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
+        for i in range(3, n + 1):
+            self.hashmap[i] = self.hashmap[i - 1] + self.hashmap[i - 2] + self.hashmap[i - 3]
 
         return self.hashmap[n]
 
