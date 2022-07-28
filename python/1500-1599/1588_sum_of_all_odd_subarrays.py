340d43fae805fa23e9dcffdf55186630835dc732
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-28 17:51:03 +0300

    303_range_sum_query_immutable

--- python/1500-1599/1588_sum_of_all_odd_subarrays.py
+++ python/1500-1599/1588_sum_of_all_odd_subarrays.py
@@ -7,16 +7,7 @@ class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         possible_combinations = [i for i in range(1, len(arr)+1) if i % 2 == 1]
 
-        response = 0
-
-        for length in possible_combinations:
-            for i in range(len(arr)):
-                if i + length <= len(arr):
-                    response = response + sum(arr[i:i+length])
-                else:
-                    break
-
-        return response
+        return sum([sum(arr[i:i+length]) for length in possible_combinations for i in range(len(arr) - length + 1)])
 
 solution = Solution()
 print(solution.sumOddLengthSubarrays(arr = [1,4,2,5,3]))
