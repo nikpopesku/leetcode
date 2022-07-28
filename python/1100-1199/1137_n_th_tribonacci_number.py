5e58d756e708507f9ca12c6fd3638c0494878b8b
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-28 18:54:07 +0300

    1137_n_th_tribonacci_number

--- python/1100-1199/1137_n_th_tribonacci_number.py
+++ python/1100-1199/1137_n_th_tribonacci_number.py
@@ -2,7 +2,7 @@ from typing import List
 from functools import reduce
 
 class Solution:
-    def __init(self):
+    def __init__(self):
         self.hashmap = {0: 0, 1: 1, 2: 1}
 
     def tribonacci(self, n: int) -> int:
