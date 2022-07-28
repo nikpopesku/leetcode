b9e71cf631d34baaee2e5df1e3c0544deb34379e
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-28 19:06:23 +0300

    746_min_cost_climbing_stairs

---
+++ python/700-799/746_min_cost_climbing_stairs.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List, Set
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        return 5
+
+
+solution = Solution()
+print(solution.minCostClimbingStairs(cost = [10,15,20]))
