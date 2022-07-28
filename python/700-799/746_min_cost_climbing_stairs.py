ec7992db8bdcbb0dada326f873e4d5c8a50856c0
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-28 19:22:09 +0300

    746_min_cost_climbing_stairs

--- python/700-799/746_min_cost_climbing_stairs.py
+++ python/700-799/746_min_cost_climbing_stairs.py
@@ -3,8 +3,18 @@ from typing import List, Set
 
 
 class Solution:
+    def __init__(self):
+        self.hashmap = {}
+
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        return 5
+        if len(cost) in [1, 2]:
+            return min(cost)
+
+        if len(cost) not in self.hashmap:
+            self.hashmap[len(cost)] = min(cost[-1] + self.minCostClimbingStairs(cost[:-1]),
+                                          cost[-2] + self.minCostClimbingStairs(cost[:-2]))
+
+        return self.hashmap[len(cost)]
 
 
 solution = Solution()
