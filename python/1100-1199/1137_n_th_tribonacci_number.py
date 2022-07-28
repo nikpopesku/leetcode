ce6c56cea93199b7c7a774bb85a80b9067d72e6a
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-28 18:52:47 +0300

    1137_n_th_tribonacci_number

--- python/1100-1199/1137_n_th_tribonacci_number.py
+++ python/1100-1199/1137_n_th_tribonacci_number.py
@@ -2,8 +2,14 @@ from typing import List
 from functools import reduce
 
 class Solution:
+    def __init(self):
+        self.hashmap = {0: 0, 1: 1, 2: 1}
+
     def tribonacci(self, n: int) -> int:
-        return 5
+        if n not in self.hashmap:
+            self.hashmap[n] = self.tribonacci(n - 1) + self.tribonacci(n - 2) + self.tribonacci(n - 3)
+
+        return self.hashmap[n]
 
 solution = Solution()
-print(solution.tribonacci(n = 234))
+print(solution.tribonacci(n = 0))
