c791c854b9a8aeb31167c4398026f59d4b9a99ca
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-29 16:15:59 +0300

    213_house_robber_2

---
+++ python/200-299/213_house_robber_2.py
@@ -0,0 +1,13 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return 4
+
+
+
+
+
+solution = Solution()
+print(solution.rob(nums = [2]))
