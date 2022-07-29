7bf9d931101d83387acb527f4816cd4e1fcbac4d
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-29 16:14:37 +0300

    UN-19530: Prevent sending emails for churned companies

--- python/100-199/198_house_robber.py
+++ python/100-199/198_house_robber.py
@@ -3,26 +3,18 @@ from typing import List, Set
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        houses = {}
-        length = len(nums)
+        houses = {1: nums[0]}
 
-        if length == 1:
-            return nums[0]
+        houses[2] = nums[1] if len(nums) > 1 and nums[1] > nums[0] else nums[0]
 
-        if length == 2:
-            return nums[0] if nums[0] > nums[1] else nums[1]
-
-        houses[1] = nums[0]
-        houses[2] = nums[0] if nums[0] > nums[1] else nums[1]
-
-        for i in range(3, length+1):
+        for i in range(3, len(nums)+1):
             houses[i] = max(houses[i-2] + nums[i-1], houses[i-1])
 
-        return houses[length]
+        return houses[len(nums)]
 
 
 
 
 
 solution = Solution()
-print(solution.rob(nums = [183,219,57,193,94,233,202,154,65,240,97,234,100,249,186,66,90,238,168,128,177,235,50,81,185,165,217,207,88,80,112,78,135,62,228,247,211]))
+print(solution.rob(nums = [2]))
