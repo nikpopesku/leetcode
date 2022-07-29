190eba2c96938004317bb46e6910943bd44f4337
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-29 16:47:11 +0300

    740_delete_and_earn

---
+++ python/200-299/740_delete_and_earn.py
@@ -0,0 +1,13 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        return 5
+
+
+
+
+
+solution = Solution()
+print(solution.deleteAndEarn(nums = [3,4,2]))
