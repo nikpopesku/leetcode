7679a5e4cdadfb1fa048e8e1ad2547c5833b84fd
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-30 12:26:38 +0300

    740_delete_and_earn

--- python/200-299/740_delete_and_earn.py
+++ python/200-299/740_delete_and_earn.py
@@ -14,7 +14,25 @@ class Solution:
 
 
 
+class Solution2:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        max_points = {}
 
+        for i in range(len(nums)):
+            max_points[nums[i]] = nums[i] if nums[i] not in max_points else max_points[nums[i]] + nums[i]
 
-solution = Solution()
+        max_points = {k: v for k, v in sorted(max_points.items(), key=lambda item: item[1], reverse=True)}
+
+        response = 0
+        used = []
+
+        for k,v in max_points.items():
+            if k not in used:
+                used.append(k+1)
+                used.append(k-1)
+                response = response + v
+
+        return response
+
+solution = Solution2()
 print(solution.deleteAndEarn(nums = [3,4,2]))
