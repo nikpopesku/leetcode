d86bde778e72a2004378fbd72b9d013020403f07
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-30 19:57:03 +0300

    740_delete_and_earn

---
+++ python/0-99/55_jump_game.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        return True
+
+
+
+solution = Solution()
+print(solution.canJump(nums = [2,3,1,1,4]))
