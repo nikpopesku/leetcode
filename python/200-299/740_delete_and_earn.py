ca0cd602f616622e95057aafeb54ea2a30daffe5
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-30 19:14:18 +0300

    740_delete_and_earn

--- python/200-299/740_delete_and_earn.py
+++ python/200-299/740_delete_and_earn.py
@@ -1,35 +1,26 @@
 from typing import List
-from collections import Counter
 
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
+        max_points = {}
 
-        if len(nums) == 1:
-            return nums[0]
+        for i in range(len(nums)):
+            max_points[nums[i]] = 1 if nums[i] not in max_points else max_points[nums[i]] + 1
 
-        return max(nums[0] + self.deleteAndEarn([nums[j] for j in range(1, len(nums)) if nums[j] != nums[0] - 1 and nums[j] != nums[0] + 1]),
-                   self.deleteAndEarn(nums[1:]))
-
-
-
-class Solution2:
-    def deleteAndEarn(self, nums: List[int]) -> int:
-        freq = Counter(nums)
-        pairs = [(num, count) for num, count in freq.items()]
-        pairs.sort()
+        max_points = [(k,v) for k, v in sorted(max_points.items(), key=lambda item: item[0])]
 
         used, not_used = 0, 0
 
-        for i, (num, count) in enumerate(pairs):
-            if i == 0 or pairs[i - 1][0] != num - 1:  # no previous noum or not num - 1
+        for i, (num, count) in enumerate(max_points):
+            if i == 0 or max_points[i - 1][0] != num - 1:  # no previous num or not num - 1
                 not_used = max(used, not_used)  # choose max
-                used = num * count + not_used  # add point from this num
+                used = count * num + not_used  # add point from this num
             else:
-                used, not_used = num * count + not_used, max(used, not_used)
+                temp = not_used
+                not_used = max(used, not_used)
+                used = count * num + temp
 
         return max(used, not_used)
 
-solution = Solution2()
+solution = Solution()
 print(solution.deleteAndEarn(nums = [8,3,4,7,6,6,9,2,5,8,2,4,9,5,9,1,5,7,1,4]))
