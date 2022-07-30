f0d6b0ab08a1d6d7fe6a533f5d11e22b4aac40eb
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-30 20:45:04 +0300

    55_jump_game

--- python/0-99/55_jump_game.py
+++ python/0-99/55_jump_game.py
@@ -3,7 +3,24 @@ from typing import List, Set
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        return True
+        if len(nums) == 1:
+            return True
+
+        possible_positions = list(range(nums[0] + 1))
+        i = 1
+
+        if possible_positions[-1] >= len(nums) - 1:
+            return True
+
+        while i < len(possible_positions):
+            for j in range(1, nums[i] + 1):
+                if j + i not in possible_positions:
+                    possible_positions.append(j + i)
+                    if j + i >= len(nums) - 1:
+                        return True
+            i = i + 1
+
+        return False
 
 
 
