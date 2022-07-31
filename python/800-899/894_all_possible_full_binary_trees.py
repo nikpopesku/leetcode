1dc3c9ca3103f70e14f2372e83c7336403b3121b
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-31 12:08:38 +0300

    894_all_possible_full_binary_trees

---
+++ python/800-899/894_all_possible_full_binary_trees.py
@@ -0,0 +1,9 @@
+from typing import Optional, List
+from treenode import TreeNode
+
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        return []
+
+solution = Solution()
+print(solution.allPossibleFBT(n = 7))
