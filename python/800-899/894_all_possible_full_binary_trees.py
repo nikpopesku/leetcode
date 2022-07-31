bef53166b13056dbda88f2a53303312920c1ae9f
npopesku <nikolay.popesku@unbiased.co.uk>
2022-07-31 12:16:20 +0300

    894_all_possible_full_binary_trees

--- python/800-899/894_all_possible_full_binary_trees.py
+++ python/800-899/894_all_possible_full_binary_trees.py
@@ -3,7 +3,21 @@ from treenode import TreeNode
 
 class Solution:
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
-        return []
+        response = []
+        i = 1
+        intermediate_head = None
+        while i <= n:
+            node = TreeNode()
+            if i == 1:
+                intemediate_head = node
+                i = i + 1
+
+            if i < n - 1:
+                node.left = TreeNode()
+                node.right = TreeNode()
+                i = i + 2
+
+        return response
 
 solution = Solution()
 print(solution.allPossibleFBT(n = 7))
