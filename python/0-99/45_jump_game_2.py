612ece76ddb82c8133f31a54bd522e6edae4d30c
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-01 18:03:31 +0300

    45_jump_game_2

---
+++ python/0-99/45_jump_game_2.py
@@ -0,0 +1,12 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+
+        return False
+
+
+
+solution = Solution()
+print(solution.jump(nums = [1,1,1,0]))
