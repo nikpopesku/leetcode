d10e3e88d5c77a459c0705debeb1ad5fe7016474
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-02 21:49:57 +0300

    45_jump_game_2

--- python/0-99/45_jump_game_2.py
+++ python/0-99/45_jump_game_2.py
@@ -3,24 +3,20 @@ from typing import List, Set
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        start, end = 0, 1
-        max_index = 0
-        step = 1
-
         if len(nums) == 1:
             return 0
 
+        start, end = 0, 0  # indices in nums of current range
+        max_index = 0
+        steps = 1
 
-        while True:
-            for k in range(start, end+1):
-                if nums[k] + k > max_index:
-                    max_index = nums[k] + k
-
-                if max_index >= len(nums) - 1:
-                    return step
-
-                step = step + 1
-                start, end = end, max_index + 1
+        while True:  # will always terminate since last index is accessible
+            for i in range(start, end + 1):
+                max_index = max(max_index, i + nums[i])
+            if max_index >= len(nums) - 1:
+                return steps
+            steps += 1
+            start, end = end + 1, max_index
 
 
 
