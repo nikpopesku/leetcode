946861036ce4504cfd16d54045e63a7d6707bd69
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-02 21:43:00 +0300

    45_jump_game_2

--- python/0-99/45_jump_game_2.py
+++ python/0-99/45_jump_game_2.py
@@ -3,10 +3,24 @@ from typing import List, Set
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        start, end = 0, 1
+        max_index = 0
+        step = 0
 
-        return False
+
+        while True:
+            for k in range(start, end):
+                if nums[k] + k > max_index:
+                    step = step + 1
+                    max_index = nums[k] + k
+
+                if max_index >= len(nums) - 1:
+                    return step
+
+                start, end = end, max_index + 1
+
 
 
 
 solution = Solution()
-print(solution.jump(nums = [1,1,1,0]))
+print(solution.jump(nums = [1]))
