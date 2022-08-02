c923f51af80a079581b66e9aaab648998cbd4688
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-02 22:48:23 +0300

    918_maximum_sum_circular_subarray

--- python/900-999/918_maximum_sum_circular_subarray.py
+++ python/900-999/918_maximum_sum_circular_subarray.py
@@ -8,17 +8,16 @@ class Solution:
         max_so_far = None
         flag_beginning = False
         i = 0
+        start_step = 0
 
         while True:
             max_current += nums[i]
 
+            if i == start_step and flag_beginning is True:
+                break
+
             if max_so_far is None or max_current > max_so_far:
                 max_so_far = max_current
-
-            if max_current < 0:
-                max_current = 0
-                if flag_beginning is True:
-                    break
 
             i += 1
 
@@ -26,8 +25,12 @@ class Solution:
                 flag_beginning = True
                 i = i % len(nums)
 
+            if max_current < 0 or nums[i] < 0:
+                max_current = 0
+                start_step = i
+
         return max_so_far
 
 
 solution = Solution()
-print(solution.maxSubarraySumCircular(nums = [1,-2,3,-2]))
+print(solution.maxSubarraySumCircular(nums = [-3,-2,-3]))
