e96d8942cfdc6a8ca1e3689da7088a6e31685d54
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-03 17:36:13 +0300

    1014_best_sightseeing_pair

---
+++ python/1000-1099/1014_best_sightseeing_pair.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        return 5
+
+
+solution = Solution()
+print(solution.maxScoreSightseeingPair(values = [8,1,5,2,6]))
