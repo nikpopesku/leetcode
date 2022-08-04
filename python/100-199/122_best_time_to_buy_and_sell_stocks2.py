b3fa0c41cffb1ac0613965c0b81548056ecf464e
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-04 16:59:38 +0300

    122_best_time_to_buy_and_sell_stocks2

---
+++ python/100-199/122_best_time_to_buy_and_sell_stocks2.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return 4
+
+solution = Solution()
+print(solution.maxProfit(prices = [7,1,5,3,6,4]))
