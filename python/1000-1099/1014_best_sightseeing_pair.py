467b0003314b6e1dedebe9cda97fcf455ae4033c
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-04 16:48:19 +0300

    1014_best_sightseeing_pair

--- python/1000-1099/1014_best_sightseeing_pair.py
+++ python/1000-1099/1014_best_sightseeing_pair.py
@@ -4,32 +4,14 @@ from typing import List
 
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        maxstart = values[1] + 1
-        max_place = 1
+        response = 0
+        maxi = 0
 
-        for i in range(2, len(values) - 1):
-            if values[i] > maxstart:
-                maxstart = values[i] + i
-                max_place = i
+        for k in range(len(values)):
+            response = max(response, maxi + values[k] - k)
+            maxi = max(maxi, values[k] + k)
 
-        maxend1 = None
-        for j in range(max_place + 1, len(values)):
-            if maxend1 is None or maxend1 < values[j] - j:
-                maxend1 = values[j] - j
-
-        maxend2 = None
-        for j in range(max_place):
-            if maxend2 is None or maxend2 < values[j] + j:
-                maxend2 = values[j] + j
-
-
-        if maxend1 is None:
-            return maxend2 + maxstart - max_place * 2
-
-        if maxend2 is None:
-            return maxstart + maxend1
-
-        return max(maxstart + maxend1, maxend2 + maxstart - max_place * 2)
+        return response
 
 
 solution = Solution()
