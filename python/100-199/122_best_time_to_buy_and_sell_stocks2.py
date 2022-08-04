64582e62c28c22025179d60f8f4f5676b50f478e
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-04 17:19:58 +0300

    122_best_time_to_buy_and_sell_stocks2

--- python/100-199/122_best_time_to_buy_and_sell_stocks2.py
+++ python/100-199/122_best_time_to_buy_and_sell_stocks2.py
@@ -3,7 +3,28 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        return 4
+        local_minimum = None
+        local_maximum = None
+        response = 0
+
+        for i in range(len(prices)):
+            if local_minimum is None or local_minimum > prices[i]:
+                if local_minimum is not None and local_maximum is not None and local_maximum > local_minimum:
+                    response = response + local_maximum - local_minimum
+                local_minimum = prices[i]
+                local_maximum = prices[i]
+
+            if local_maximum < prices[i]:
+                local_maximum = prices[i]
+                if i == len(prices) - 1 and local_maximum > local_minimum:
+                    response = response + local_maximum - local_minimum
+
+            if local_maximum > prices[i] and local_maximum > local_minimum:
+                response = response + local_maximum - local_minimum
+                local_minimum = prices[i]
+                local_maximum = prices[i]
+
+        return response
 
 solution = Solution()
-print(solution.maxProfit(prices = [7,1,5,3,6,4]))
+print(solution.maxProfit(prices = [2,4,1]))
