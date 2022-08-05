3d7c1d5cb145951adce812411354a9fccccd6749
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-05 16:16:26 +0300

    309_best_time_to_buy_and_sell_with_cooldown

---
+++ python/300-399/309_best_time_to_buy_and_sell_with_cooldown.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return 5
+
+
+solution = Solution()
+print(solution.maxProfit(prices = [1,2,3,0,2]))
