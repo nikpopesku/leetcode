e890b6c02612fe55ba91e9241ac2b515ee197fea
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-05 16:32:49 +0300

    309_best_time_to_buy_and_sell_with_cooldown

--- python/300-399/309_best_time_to_buy_and_sell_with_cooldown.py
+++ python/300-399/309_best_time_to_buy_and_sell_with_cooldown.py
@@ -4,7 +4,27 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        return 5
+
+        if len(prices) <= 1:
+            return 0
+
+        has_cooldown = False
+        profit = 0
+        last_price = prices[0]
+
+        for k in range(1, len(prices)):
+            if prices[k] > last_price and not has_cooldown:
+                profit = profit + prices[k] - last_price
+                has_cooldown = True
+                last_price = prices[k]
+                continue
+
+            last_price = prices[k]
+
+            if has_cooldown:
+                has_cooldown = False
+
+        return profit
 
 
 solution = Solution()
