f110cdc96b7d135bf43e2271a7105cb98aab4456
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-05 16:39:30 +0300

    309_best_time_to_buy_and_sell_with_cooldown

--- python/300-399/309_best_time_to_buy_and_sell_with_cooldown.py
+++ python/300-399/309_best_time_to_buy_and_sell_with_cooldown.py
@@ -8,18 +8,20 @@ class Solution:
         if len(prices) <= 1:
             return 0
 
+        profit, local_max, local_min = 0, prices[0], prices[0]
         has_cooldown = False
-        profit = 0
-        last_price = prices[0]
 
         for k in range(1, len(prices)):
-            if prices[k] > last_price and not has_cooldown:
-                profit = profit + prices[k] - last_price
-                has_cooldown = True
-                last_price = prices[k]
-                continue
+            if prices[k] > local_max:
+                local_max = prices[k]
 
-            last_price = prices[k]
+            if prices[k] < local_max and not has_cooldown and local_max > local_min:
+                profit = profit + local_max - local_min
+                has_cooldown = True
+
+            if prices[k] < local_min:
+                local_min = prices[k]
+                local_max = prices[k]
 
             if has_cooldown:
                 has_cooldown = False
