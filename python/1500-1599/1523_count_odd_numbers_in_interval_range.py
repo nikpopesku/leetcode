43c32e1fff67fa3ae46a4764c2563e762a4c483e
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-06 08:07:07 +0300

    fix

--- python/1500-1599/1523_count_odd_numbers_in_interval_range.py
+++ python/1500-1599/1523_count_odd_numbers_in_interval_range.py
@@ -2,15 +2,8 @@ from typing import List
 
 
 class Solution:
-    def average(self, salary: List[int]) -> float:
-        minimum, maximum, sum = salary[0], salary[0], salary[0]
-
-        for i in range(1, len(salary)):
-            minimum = min(minimum, salary[i])
-            maximum = max(maximum, salary[i])
-            sum += salary[i]
-
-        return round(((sum - minimum - maximum) / (len(salary) - 2)), 5)
+    def countOdds(self, low: int, high: int) -> int:
+        return (high - low) // 2 + (1 if low % 2 == 1 or high % 2 == 1 else 0)
 
 solution = Solution()
-print(solution.average(salary = [4000,3000,1000,2000]))
+print(solution.countOdds(salary = [4000,3000,1000,2000]))
