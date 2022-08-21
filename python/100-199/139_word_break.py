9bd57a191ede35bbc50ac9572fb77a9f81fef3b6
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-21 22:40:34 +0700

    139_word_break

---
+++ python/100-199/139_word_break.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        return True
+
+
+solution = Solution()
+print(solution.wordBreak(s = "leetcode", wordDict = ["leet","code"]))
