82d0290d07ccca64e1eb1b5894571595649bd251
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-21 22:57:10 +0700

    139_word_break

--- python/100-199/139_word_break.py
+++ python/100-199/139_word_break.py
@@ -3,7 +3,13 @@ from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        return True
+        for word in wordDict:
+            if s[:len(word)] == word:
+                if len(word) == len(s):
+                    return True
+                return self.wordBreak(s[len(word):], wordDict)
+
+        return False
 
 
 solution = Solution()
