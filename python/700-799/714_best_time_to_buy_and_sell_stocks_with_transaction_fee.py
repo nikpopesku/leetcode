cc7706653243a9812ec89b858cf3a52877a8ef2b
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-21 20:59:56 +0700

    714_best_time_to_buy_and_sell_stocks_with_transaction_fee

--- python/700-799/714_best_time_to_buy_and_sell_stocks_with_transaction_fee.py
+++ python/700-799/714_best_time_to_buy_and_sell_stocks_with_transaction_fee.py
@@ -3,16 +3,16 @@ from typing import List
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
         buy, sell, prev_sell = float("-inf"), 0, 0
 
         for i, price in enumerate(prices):
-            buy = max(buy, prev_sell - price)
+            buy = max(buy, prev_sell - price - fee)
             prev_sell = sell
-            sell = max(sell, buy + price)
+            sell = max(sell, buy + price - fee)
 
         return sell
 
 
 solution = Solution()
-print(solution.maxProfit(prices = [1,2,3,0,2]))
+print(solution.maxProfit(prices = [1,3,2,8,4,9], fee = 2))
