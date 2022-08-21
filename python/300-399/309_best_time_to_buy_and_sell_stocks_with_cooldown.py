90f3a24c21f4551e8fbaee7b948257dc9533f82d
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-21 20:51:29 +0700

    309_best_time_to_buy_and_sell_stocks_with_cooldown

--- python/300-399/309_best_time_to_buy_and_sell_stocks_with_cooldown.py
+++ python/300-399/309_best_time_to_buy_and_sell_stocks_with_cooldown.py
@@ -4,7 +4,15 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        return 3
+        buy, sell, prev_sell = float("-inf"), 0, 0
+
+        for i, price in enumerate(prices):
+            buy = max(buy, prev_sell - price)
+            prev_sell = sell
+            sell = max(sell, buy + price)
+
+        return sell
+
 
 solution = Solution()
 print(solution.maxProfit(prices = [1,2,3,0,2]))
