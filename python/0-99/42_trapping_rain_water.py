ba9b903a71a009795b887f812f9ce6f8267cdb37
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-24 09:37:23 +0700

    42_trapping_rain_water

---
+++ python/0-99/42_trapping_rain_water.py
@@ -0,0 +1,23 @@
+import sys
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        water, max_left, max_right = 0, height[0], height[len(height) - 1]
+        i, j = 0, len(height) - 1
+
+        while i < j:
+            if max_left < max_right:
+                max_left = max(max_left, height[i])
+                water += max_left - height[i]
+                i += 1
+            else:
+                max_right = max(max_right, height[j])
+                water += max_right - height[j]
+                j -= 1
+
+        return water
+
+
+solution = Solution()
+print(solution.trap(height = [0,1,0,2,1,0,1,3,2,1,2,1]))
