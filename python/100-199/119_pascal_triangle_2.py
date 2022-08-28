f029a56c6c413ec100725fc14f397ba654ad9a6e
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-28 19:56:17 +0700

    119_pascal_triangle_2

---
+++ python/100-199/119_pascal_triangle_2.py
@@ -0,0 +1,10 @@
+from typing import Optional, List
+from treenode import TreeNode
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        return [1,2,3]
+
+
+solution = Solution()
+print(solution.getRow(rowIndex = 3))
