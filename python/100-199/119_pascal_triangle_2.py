a82be71a14b4a9a0b7cf5529a7ac24eb2a88eda2
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-28 21:02:07 +0700

    119_pascal_triangle_2

--- python/100-199/119_pascal_triangle_2.py
+++ python/100-199/119_pascal_triangle_2.py
@@ -3,7 +3,15 @@ from treenode import TreeNode
 
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        return [1,2,3]
+        dp = [(1,), (1, 1), (1, 2, 1)]
+
+        if rowIndex <= len(dp) - 1:
+            return list(dp[rowIndex])
+
+        for i in range(3, rowIndex + 1):
+            dp.append(tuple(1 if j == 0 or j == len(dp[i - 1]) else dp[i - 1][j - 1] + dp[i - 1][j] for j in range(i + 1)))
+
+        return list(dp[-1])
 
 
 solution = Solution()
