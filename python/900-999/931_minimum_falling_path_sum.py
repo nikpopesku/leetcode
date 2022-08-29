1f4026a637dad2f6ac2495282d72fff4a16c4f50
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-29 09:40:52 +0700

    931_minimum_falling_path_sum

---
+++ python/900-999/931_minimum_falling_path_sum.py
@@ -0,0 +1,10 @@
+from typing import List, Dict
+
+
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        return 13
+
+
+solution = Solution()
+print(solution.minFallingPathSum(matrix = [[2,1,3],[6,5,4],[7,8,9]]))
