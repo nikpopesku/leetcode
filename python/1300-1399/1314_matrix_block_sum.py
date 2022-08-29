10ece6173c5fe4a654e771b9740f892ca735f2e1
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-29 10:43:08 +0700

    1314_matrix_block_sum

--- python/1300-1399/1314_matrix_block_sum.py
+++ python/1300-1399/1314_matrix_block_sum.py
@@ -4,7 +4,7 @@ from functools import reduce
 
 class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
-        return [[sum(mat[i+k0][j+k1] for k0 in range(-k, k+1) for k1 in range(-k, k+1) if 0 <= i + k0 <= len(mat) - 1 and 0 <= j + k1 <= len(mat) - 1) for j in range(len(mat))] for i in range(len(mat))]
+        return [[sum(mat[i+k0][j+k1] for k0 in range(-k, k+1) for k1 in range(-k, k+1) if 0 <= i + k0 <= len(mat) - 1 and 0 <= j + k1 <= len(mat[0]) - 1) for j in range(len(mat))] for i in range(len(mat))]
 
 solution = Solution()
 print(solution.matrixBlockSum(mat = [[67,64,78],[99,98,38],[82,46,46],[6,52,55],[55,99,45]], k = 3))
