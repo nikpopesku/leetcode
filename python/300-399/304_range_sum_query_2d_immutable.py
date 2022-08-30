2a29d952b69916b07293d939b3af6c26aecb523b
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-30 12:02:23 +0700

    304_range_sum_query_2d_immutable

--- python/300-399/304_range_sum_query_2d_immutable.py
+++ python/300-399/304_range_sum_query_2d_immutable.py
@@ -5,10 +5,19 @@ from typing import List
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        pass
+        self.matrix = matrix
+        self.sums = {}
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return 5
+        total = 0
+
+        for row in range(row1, row2 + 1):
+            if (row, col1, col2) not in self.sums:
+                self.sums[(row, col1, col2)] = sum(self.matrix[row][col1:col2 + 1])
+
+            total += self.sums[(row, col1, col2)]
+
+        return total
 
 obj = NumArray(nums)
 param_1 = obj.sumRange(left,right)
