5afff1c56ead982780c0d95e86e1a0e24211fb8b
npopesku <nikolay.popesku@unbiased.co.uk>
2022-08-30 10:26:28 +0700

    1314_matrix_block_sum

--- python/1300-1399/1314_matrix_block_sum.py
+++ python/1300-1399/1314_matrix_block_sum.py
@@ -1,10 +1,36 @@
 from typing import List
 from functools import reduce
 
+def solveRow(row, k):
+    '''
+    O(n)
+    Where n is the number of elements in the row
+    '''
+    n = len(row)
+    k = min(k, n)
+    newRow = []
+    right = k
+    left = 0
+    total = sum(row[0: right + 1])
+    newRow.append(total)
+
+    for index in range(1, n):
+        if index > k:
+            total -= row[left]
+            left += 1
+
+        if index < n - k:
+            right += 1
+            total += row[right]
+        newRow.append(total)
+
+    return newRow
 
 class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
-        return [[sum(mat[i+k0][j+k1] for k0 in range(-k, k+1) for k1 in range(-k, k+1) if 0 <= i + k0 <= len(mat) - 1 and 0 <= j + k1 <= len(mat[0]) - 1) for j in range(len(mat[0]))] for i in range(len(mat))]
+        ans = [solveRow(row,k) for row in mat]   # O(n*m)
+        ans = [solveRow(x, k) for x in zip(*ans)] # O(n*m)  Transpose
+        return [list(x) for x in zip(*ans)] # Transpose again
 
 solution = Solution()
 print(solution.matrixBlockSum(mat = [[67,64,78],[99,98,38],[82,46,46],[6,52,55],[55,99,45]], k = 3))
