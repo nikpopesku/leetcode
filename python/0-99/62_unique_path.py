f05e9845284b5c4e12ca32e9ef92fef73cde6674
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-01 16:04:55 +0700

    62_unique_path

--- python/0-99/62_unique_path.py
+++ python/0-99/62_unique_path.py
@@ -4,16 +4,18 @@ from typing import List
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = {(0, 0): 1}
+        dp = [[0 for j in range(n)] for i in range(m)]
 
         for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0:
-                    continue
-                dp[(i, j)] = (dp[(i - 1, j)] if i > 0 else 0) + (dp[(i, j - 1)] if j > 0 else 0)
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] = 1
 
-        return dp[(m - 1, n - 1)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
 
+        return dp[m - 1][n - 1]
 
 solution = Solution()
 print(solution.uniquePaths(m = 3, n = 7))
