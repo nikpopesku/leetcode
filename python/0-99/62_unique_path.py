a689fc53a05452e148ce91c1ac11ca93db354973
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-01 14:55:34 +0700

    62_unique_path

---
+++ python/0-99/62_unique_path.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        return 28
+
+
+solution = Solution()
+print(solution.uniquePaths(m = 3, n = 7))
