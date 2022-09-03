93e0f9c87aaae7f6c49a104c06efda6e3b863717
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-03 11:05:11 +0700

    63_unique_path_2

--- python/0-99/63_unique_path_2.py
+++ python/0-99/63_unique_path_2.py
@@ -4,7 +4,30 @@ from typing import List
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        return 4
+        if obstacleGrid[-1][-1] == 1:
+            return 0
+
+        obstacle = False
+        for i in range(len(obstacleGrid)):
+            obstacle = obstacle or obstacleGrid[i][0] == 1
+            obstacleGrid[i][0] = 1 if obstacle else -1
+
+        obstacle = False
+        for i in range(len(obstacleGrid[0])):
+            obstacle = obstacle or obstacleGrid[0][i] == 1
+            obstacleGrid[0][i] = 1 if obstacle else -1
+
+        for i in range(1, len(obstacleGrid)):
+            for j in range(1, len(obstacleGrid[i])):
+                if obstacleGrid[i][j] == 1:
+                    continue
+                elif obstacleGrid[i - 1][j] == 1 and obstacleGrid[i][j - 1] == 1:
+                    obstacleGrid[i][j] = 1
+                else:
+                    obstacleGrid[i][j] = (obstacleGrid[i - 1][j] if obstacleGrid[i - 1][j] < 0 else 0) + (
+                        obstacleGrid[i][j - 1] if obstacleGrid[i][j - 1] < 0 else 0)
+
+        return abs(obstacleGrid[-1][-1]) if obstacleGrid[-1][-1] < 0 else 0
 
 
 solution = Solution()
