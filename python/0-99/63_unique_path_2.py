65dea449239a1c1a86d9f13de0f719ab4be8538f
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-03 10:42:23 +0700

    63_unique_path_2

--- python/0-99/63_unique_path_2.py
+++ python/0-99/63_unique_path_2.py
@@ -3,19 +3,9 @@ from typing import List
 
 
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0 for j in range(n)] for i in range(m)]
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        return 4
 
-        for i in range(m):
-            dp[i][0] = 1
-        for j in range(n):
-            dp[0][j] = 1
-
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-
-        return dp[m - 1][n - 1]
 
 solution = Solution()
-print(solution.uniquePaths(m = 3, n = 7))
+print(solution.uniquePathsWithObstacles(obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]))
