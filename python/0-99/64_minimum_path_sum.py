40828fa558d88bc762d7a84a6e7a99554b324583
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-03 16:25:13 +0700

    63_unique_path_2

---
+++ python/0-99/64_minimum_path_sum.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        return 5
+
+
+solution = Solution()
+print(solution.minPathSum(grid = [[1,3,1],[1,5,1],[4,2,1]]))
