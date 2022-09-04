0e550793d286ce9c9dbefe27537e327e7b3d2379
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-04 18:39:03 +0700

    221_maximal_square

--- python/200-299/221_maximal_square.py
+++ python/200-299/221_maximal_square.py
@@ -4,14 +4,13 @@ from typing import List, Set
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         dp = [int(matrix[0][j]) for j in range(len(matrix[0]))]
-        max_dp = float('-inf')
+        max_dp = max(dp)
 
         for i in range(1, len(matrix)):
             element = [int(matrix[i][0])]
             for j in range(1, len(matrix[i])):
                 if (matrix[i - 1][j - 1] == '1'):
-                    current = int(matrix[i][j])
-                    element.append(min(dp[i - 1][j - 1], dp[i - 1][j], element[-1]) + current)
+                    element.append(min(dp[j - 1], dp[j], element[-1]) + int(matrix[i][j]))
                 else:
                     element.append(int(matrix[i][j]))
 
@@ -22,4 +21,4 @@ class Solution:
 
 
 solution = Solution()
-print(solution.maximalSquare(matrix = [["0","1"],["1","0"]]))
+print(solution.maximalSquare(matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]))
