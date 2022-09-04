47d0db17bc6860579829ab81958170643529e888
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-04 18:33:44 +0700

    221_maximal_square

--- python/200-299/221_maximal_square.py
+++ python/200-299/221_maximal_square.py
@@ -3,7 +3,22 @@ from typing import List, Set
 
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        return 1
+        dp = [int(matrix[0][j]) for j in range(len(matrix[0]))]
+        max_dp = float('-inf')
+
+        for i in range(1, len(matrix)):
+            element = [int(matrix[i][0])]
+            for j in range(1, len(matrix[i])):
+                if (matrix[i - 1][j - 1] == '1'):
+                    current = int(matrix[i][j])
+                    element.append(min(dp[i - 1][j - 1], dp[i - 1][j], element[-1]) + current)
+                else:
+                    element.append(int(matrix[i][j]))
+
+            dp = element
+            max_dp = max(*dp, max_dp)
+
+        return int(max_dp * max_dp)
 
 
 solution = Solution()
