c57b3cafa2247f3a552d37e8c6e351371d94ccbc
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-04 10:23:56 +0700

    221_maximal_square

---
+++ python/200-299/221_maximal_square.py
@@ -0,0 +1,10 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        return 1
+
+
+solution = Solution()
+print(solution.maximalSquare(matrix = [["0","1"],["1","0"]]))
