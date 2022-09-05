ee4e9c7f20a0eff94796fa1920e736856d4c68d9
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-05 10:55:09 +0700

    5_longest_palindrome_substring

--- python/0-99/5_longest_palindrome_substring.py
+++ python/0-99/5_longest_palindrome_substring.py
@@ -3,7 +3,17 @@ import sys
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        return "bab"
+        maxp = 1
+        maxsub = s[0]
+
+        for i in range(len(s)):
+            for j in range(i + maxp, len(s)-1):
+                temp = s[i:j+1]
+                if temp == temp[::-1]:
+                    maxp = j + 1 - i
+                    maxsub = s[i:j+1]
+
+        return maxsub
 
 
 solution = Solution()
