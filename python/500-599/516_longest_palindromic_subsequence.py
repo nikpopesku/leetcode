ca72efe34a04701bf2fedb52e900fe7a51989191
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-06 11:35:34 +0700

    516_longest_palindromic_subsequence

--- python/500-599/516_longest_palindromic_subsequence.py
+++ python/500-599/516_longest_palindromic_subsequence.py
@@ -4,23 +4,20 @@ from typing import List
 
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        maxl = 1
-        maxs = s[0]
-
-        for i in range(len(s)):
-            temp = None
-
-            for j in range(i + maxl, len(s)):
-                temp = s[i:j + 1] if temp is None else temp + s[j]
-
-                if temp == temp[::-1]:
-                    maxl += 1
-                    maxs = temp
+        n = len(s)
+        dp = [[1] * n for _ in range(n)]
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    if j == i + 1:
+                        dp[i][j] = 2
+                    else:
+                        dp[i][j] = 2 + dp[i + 1][j - 1]
                 else:
-                    temp = temp[:len(temp) - 1]
+                    dp[i][j] = max(dp[i][j - 1], dp[i + 1][j])
 
-        return maxl
+        return dp[0][-1]
 
 
 solution = Solution()
-print(solution.longestPalindromeSubseq(s = "bbbab"))
+print(solution.longestPalindromeSubseq(s = "aabaa"))
