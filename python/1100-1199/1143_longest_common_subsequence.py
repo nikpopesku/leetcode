e8f24d57f68523cd71497e7ac443517ba5d2d0d8
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-09 18:12:24 +0700

    1143_longest_common_subsequence

---
+++ python/1100-1199/1143_longest_common_subsequence.py
@@ -0,0 +1,11 @@
+from typing import List
+from functools import reduce
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        return 3
+
+
+solution = Solution()
+print(solution.longestCommonSubsequence(text1 = "abcde", text2 = "ace" ))
