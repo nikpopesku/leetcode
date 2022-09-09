ecb0d7527cfae3bb2ff3853493d6fe1280b69cec
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-09 20:29:53 +0700

    1143_longest_common_subsequence

--- python/1100-1199/1143_longest_common_subsequence.py
+++ python/1100-1199/1143_longest_common_subsequence.py
@@ -4,49 +4,20 @@ from functools import reduce
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp_max = {}
-        dp_min = {}
+        l1 = len(text1)
+        l2 = len(text2)
 
-        min_length = min(len(text1), len(text2))
-        min_text = text1 if len(text1) == min_length else text2
-        max_text = text2 if min_text == text1 else text1
+        dp = [[0] * (l2 + 1) for _ in range(l1 + 1)]
 
-        min_so_far = float('inf')
-        min_index = None
+        for row in range(1, l1 + 1):
+            for col in range(1, l2 + 1):
+                if text1[row - 1] == text2[col - 1]:
+                    dp[row][col] = dp[row - 1][col - 1] + 1
+                else:
+                    dp[row][col] = max(dp[row - 1][col], dp[row][col - 1])
 
-        for i in range(len(min_text)):
-            index = max_text.find(min_text[i])
-            if index != -1 and index < min_so_far:
-                min_so_far = index
-                min_index = i
-
-        if min_so_far == float('inf'):
-            return 0
-
-        dp_max[0] = min_so_far
-        dp_min[0] = min_index
-
-        j = 0
-        while j < len(min_text)-1:
-            j += 1
-            min_so_far = float('inf')
-            min_index = None
-
-            for i in range(dp_min[j - 1] + 1, len(min_text)):
-                index = max_text.find(min_text[i], dp_max[j - 1] + 1)
-
-                if index != -1 and index < min_so_far:
-                    min_so_far = index
-                    min_index = i
-
-            if min_so_far == float('inf'):
-                return max(dp_min.keys()) + 1
-
-            dp_max[j] = min_so_far
-            dp_min[j] = min_index
-
-        return max(dp_min.keys()) + 1
+        return dp[l1][l2]
 
 
 solution = Solution()
-print(solution.longestCommonSubsequence(text1 = "abcde", text2 = "ace" ))
+print(solution.longestCommonSubsequence(text1 = "oxcpqrsvwf", text2 = "shmtulqrypy" ))
