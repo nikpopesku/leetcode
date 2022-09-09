94834954c5f984806f49cd8a33b39d9ef4e1b403
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-09 19:44:07 +0700

    1143_longest_common_subsequence

--- python/1100-1199/1143_longest_common_subsequence.py
+++ python/1100-1199/1143_longest_common_subsequence.py
@@ -4,7 +4,48 @@ from functools import reduce
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        return 3
+        dp_max = {}
+        dp_min = {}
+
+        min_length = min(len(text1), len(text2))
+        min_text = text1 if len(text1) == min_length else text2
+        max_text = text2 if min_text == text1 else text1
+
+        min_so_far = float('inf')
+        min_index = None
+
+        for i in range(len(min_text)):
+            index = max_text.find(min_text[i])
+            if index != -1 and index < min_so_far:
+                min_so_far = index
+                min_index = i
+
+        if min_so_far == float('inf'):
+            return 0
+
+        dp_max[0] = min_so_far
+        dp_min[0] = min_index
+
+        j = 0
+        while j < len(min_text):
+            j += 1
+            min_so_far = float('inf')
+            min_index = None
+
+            for i in range(dp_min[j - 1] + 1, len(min_text)):
+                index = max_text[dp_max[j - 1] + 1:].find(min_text[i])
+
+                if index != -1 and index < min_so_far:
+                    min_so_far = index
+                    min_index = i
+
+            if min_so_far == float('inf'):
+                return dp_min[j - 1]
+
+            dp_max[j] = min_so_far
+            dp_min[j] = min_index
+
+        return dp_min[j]
 
 
 solution = Solution()
