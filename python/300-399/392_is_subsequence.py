36dee44f81b6344f19caab7561d3a58749ef6163
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-09 18:06:07 +0700

    392_is_subsequence

--- python/300-399/392_is_subsequence.py
+++ python/300-399/392_is_subsequence.py
@@ -3,7 +3,21 @@ from typing import List
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        return True
+        if len(s) == 0:
+            return True
+
+        if len(t) == 0:
+            return False
+
+        counter = 0
+
+        for t0 in t:
+            if s[counter] == t0:
+                counter += 1
+                if counter == len(s):
+                    return True
+
+        return False
 
 
 solution = Solution()
