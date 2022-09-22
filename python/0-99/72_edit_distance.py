5c2f9d62fee25c6180b522c52a4a4e1949116ad0
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-22 10:18:18 +0700

    72_edit_distance

---
+++ python/0-99/72_edit_distance.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        return 4
+
+
+solution = Solution()
+print(solution.minDistance(word1 = "horse", word2 = "ros"))
