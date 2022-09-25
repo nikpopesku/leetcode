894c60a2c57167cf607bf50ac768fe33de45ae1f
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-25 13:29:46 +0700

    322_coin_change

--- python/300-399/322_coin_change.py
+++ python/300-399/322_coin_change.py
@@ -4,7 +4,21 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        return 4
+        if amount == 0:
+            return 0
+
+        if amount < min(coins):
+            return -1
+
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for c in coins:
+            for current_amount in range(1, amount + 1):
+                if current_amount >= c:
+                    dp[current_amount] = min(dp[current_amount], 1 + dp[current_amount - c])
+
+        return dp[amount] if dp[amount] != float('inf') else -1
 
 
 solution = Solution()
