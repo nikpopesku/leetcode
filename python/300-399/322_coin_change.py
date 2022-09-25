474b63266884a474417132d955b3e43e5f2b8628
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-25 12:55:28 +0700

    322_coin_change

--- python/300-399/322_coin_change.py
+++ python/300-399/322_coin_change.py
@@ -2,14 +2,10 @@ import sys
 from typing import List
 
 
-class NumArray:
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        return 4
 
-    def __init__(self, nums: List[int]):
-        self.nums = nums
 
-    def sumRange(self, left: int, right: int) -> int:
-        return sum(self.nums[left:right + 1])
-
-obj = NumArray(nums)
-param_1 = obj.sumRange(left,right)
-print(solution.reverseString(list(sys.argv[1])))
+solution = Solution()
+print(solution.coinChange(coins = [1,2,5], amount = 11))
