35135e6bfa66296a7e233198ad51309615b4b6d8
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-25 13:32:17 +0700

    322_coin_change

---
+++ python/500-599/518_coin_change2.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        return 10
+
+
+solution = Solution()
+print(solution.change(coins = [1,2,5], amount = 11))
