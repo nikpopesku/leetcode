4aa4f1e83c55158e24b1e6b8a98c903bbf02b100
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-26 21:07:11 +0700

    377_combination_sum_4

--- python/300-399/377_combination_sum_4.py
+++ python/300-399/377_combination_sum_4.py
@@ -4,7 +4,13 @@ from typing import List
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        return 4
+        dp = [0 for i in range(target + 1)]
+        dp[0] = 1
+
+        for i in range(1, target + 1):
+            dp[i] = sum(dp[i - num] for num in nums if i - num >= 0)
+
+        return dp[-1]
 
 
 solution = Solution()
