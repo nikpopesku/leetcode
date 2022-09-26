cf0add48603107e42043557233f0445ee242cc30
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-26 10:47:21 +0700

    322_coin_change

--- python/500-599/518_coin_change2.py
+++ python/500-599/518_coin_change2.py
@@ -4,7 +4,15 @@ from typing import List
 
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        return 10
+        dp = [0 for i in range(amount + 1)]
+
+        dp[0] = 1
+
+        for coin in coins:
+            for a in range(coin, amount + 1):
+                dp[a] += dp[a - coin]
+
+        return dp[-1]
 
 
 solution = Solution()
