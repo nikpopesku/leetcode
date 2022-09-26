c975a65dfe4e6f7c092668d75df982438d587d87
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-26 22:16:14 +0700

    343_integer_break

--- python/300-399/343_integer_break.py
+++ python/300-399/343_integer_break.py
@@ -4,8 +4,14 @@ from typing import List
 
 class Solution:
     def integerBreak(self, n: int) -> int:
-        return 1
+        dp = [1 for i in range(n + 1)]
+
+        for i in range(2, n + 1):
+            middle = round(i / 2)
+            dp[i] = max(max(dp[j], j) * (i - j) for j in range(middle, i))
+
+        return dp[-1]
 
 
 solution = Solution()
-print(solution.integerBreak(n = 2))
+print(solution.integerBreak(n = 10))
