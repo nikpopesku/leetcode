398634bf8bfd062a562a83436073c181fae50dbc
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-26 10:52:24 +0700

    322_coin_change

---
+++ python/300-399/377_combination_sum_4.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        return 4
+
+
+solution = Solution()
+print(solution.combinationSum4(nums = [1,2,3], target = 4))
