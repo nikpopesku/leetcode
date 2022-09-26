ab0db8c7be2ed16af0d483727cd45c9036f9242d
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-26 22:41:52 +0700

    279_perfect_squares

---
+++ python/200-299/279_perfect_squares.py
@@ -0,0 +1,13 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        return 4
+
+
+
+
+
+solution = Solution()
+print(solution.numSquares(n = 12))
