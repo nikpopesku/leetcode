4c0e18f66be058181d2403742ee8f08bf9aa7eac
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-26 21:14:52 +0700

    343_integer_break

---
+++ python/300-399/343_integer_break.py
@@ -0,0 +1,11 @@
+import sys
+from typing import List
+
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        return 1
+
+
+solution = Solution()
+print(solution.integerBreak(n = 2))
