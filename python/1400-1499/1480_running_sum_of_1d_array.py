c60db1cc7ee51e5fe76d86c455019d500007adc7
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-28 11:31:29 +0700

    1480_running_sum_of_1d_array

--- python/1400-1499/1480_running_sum_of_1d_array.py
+++ python/1400-1499/1480_running_sum_of_1d_array.py
@@ -7,6 +7,20 @@ class Solution:
         last = 0
         return [last := last + num for num in nums]
 
+def sumIt(last: int):
+    def internal(num: int) -> int:
+        nonlocal last
+        last = last + num
+
+        return last
+
+    return internal
+
+
+class Solution2:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        func = sumIt(0)
+        return [func(num) for num in nums]
 
 solution = Solution()
 print(solution.runningSum(nums = [1,2,3,4]))
