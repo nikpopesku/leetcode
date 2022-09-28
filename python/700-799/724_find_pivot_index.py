2e7855facc73f0e5b79bf29cd919e287b87d3715
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-28 11:53:41 +0700

    724_find_pivot_index

--- python/700-799/724_find_pivot_index.py
+++ python/700-799/724_find_pivot_index.py
@@ -3,24 +3,31 @@ from listnode import ListNode
 
 
 class Solution:
+    def __init__(self):
+        self.last = 0
+
+    def pivotIndex(self, nums: List[int]) -> int:
+        running_sum_left = self.runningSum(nums)
+        running_sum_right = self.runningSum(nums[::-1])
+
+        if len(nums) == 1 or running_sum_right[len(nums) - 2] == 0 or running_sum_left[:len(nums) - 2] == 0:
+            return 0
+
+        for key in range(1, len(nums) - 1):
+            if running_sum_left[key - 1] == running_sum_right[len(nums) - key - 1]:
+                return key
+
+        return -1
+
+    def sumIt(self, num: int) -> int:
+        self.last += num
+
+        return self.last
+
     def runningSum(self, nums: List[int]) -> List[int]:
-        last = 0
-        return [last := last + num for num in nums]
+        self.last = 0
+        return [self.sumIt(num) for num in nums]
 
-def sumIt(last: int):
-    def internal(num: int) -> int:
-        nonlocal last
-        last = last + num
-
-        return last
-
-    return internal
-
-
-class Solution2:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        func = sumIt(0)
-        return [func(num) for num in nums]
 
 solution = Solution()
-print(solution.runningSum(nums = [1,2,3,4]))
+print(solution.runningSum(nums = [1,7,3,6,5,6]))
