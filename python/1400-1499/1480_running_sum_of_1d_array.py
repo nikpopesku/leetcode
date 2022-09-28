c5a48613f92c4ca1c821a982ae73b173d30ac4fc
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-28 10:58:17 +0700

    279_perfect_squares

---
+++ python/1400-1499/1480_running_sum_of_1d_array.py
@@ -0,0 +1,11 @@
+from typing import List
+from listnode import ListNode
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        return [1,3,6,10]
+
+
+solution = Solution()
+print(solution.runningSum(nums = [1,2,3,4]))
