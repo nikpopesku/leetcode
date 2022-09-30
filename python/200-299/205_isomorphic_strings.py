fc7ff7ba3fd0dc9f05523fd92472d59d7bf2c7c9
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-30 10:22:45 +0700

    205_isomorphic_strings

---
+++ python/200-299/205_isomorphic_strings.py
@@ -0,0 +1,10 @@
+from typing import Optional
+from listnode import ListNode, create_single_linked_list
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return True
+
+
+solution = Solution()
+print(solution.isIsomorphic(s = "egg", t = "add"))
