a948b9d81b637c730b8b021adb6e04c5c0175d58
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-30 11:02:08 +0700

    142_linked_list_cycle_2

---
+++ python/100-199/142_linked_list_cycle_2.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from listnode import ListNode
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+
+
+
+solution = Solution()
+print(solution.nthUglyNumber(n = 268))
