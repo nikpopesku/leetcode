2ce2026aab774515ca14cf6878400fc0393269ef
npopesku <nikolay.popesku@unbiased.co.uk>
2022-09-30 10:53:04 +0700

    205_isomorphic_strings

--- python/200-299/205_isomorphic_strings.py
+++ python/200-299/205_isomorphic_strings.py
@@ -3,6 +3,18 @@ from listnode import ListNode, create_single_linked_list
 
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        map = {}
+        map_set = set()
+
+        for i in range(len(s)):
+            if s[i] not in map:
+                if t[i] in map_set:
+                    return False
+                map[s[i]] = t[i]
+                map_set.add(t[i])
+            elif map[s[i]] != t[i]:
+                return False
+
         return True
 
 
