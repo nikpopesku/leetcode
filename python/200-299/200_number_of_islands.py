ba4f978078d097bcd629b7fb16e296e277387163
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-05 21:13:49 +0700

    200_number_of_islands

--- python/200-299/200_number_of_islands.py
+++ python/200-299/200_number_of_islands.py
@@ -20,12 +20,12 @@ class Solution:
             while len(current_island) != 0:
                 i, j = (int(k) for k in current_island.pop(0).split('_'))
 
-                if i < len(grid) - 1 and grid[i + 1][j] not in visited:
+                if i < len(grid) - 1 and (str(i+1) + '_' + str(j)) not in visited:
                     visited.add(str(i + 1) + '_' + str(j))
                     if grid[i + 1][j] == '1':
                         current_island.append(str(i + 1) + '_' + str(j))
 
-                if j < len(grid[0]) - 1 and grid[i][j + 1] not in visited:
+                if j < len(grid[0]) - 1 and (str(i) + '_' + str(j+1)) not in visited:
                     visited.add(str(i) + '_' + str(j + 1))
                     if grid[i][j + 1] == '1':
                         current_island.append(str(i) + '_' + str(j + 1))
@@ -40,8 +40,8 @@ class Solution:
 
 solution = Solution()
 print(solution.numIslands(grid = [
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
   ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
 ]))
