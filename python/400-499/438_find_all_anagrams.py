11275f795c13e35ae5d29541f3e457d5a653765b
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-11 10:36:10 +0700

    438_find_all_anagrams_in_string

---
+++ python/400-499/438_find_all_anagrams.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        return [0,6]
+
+
+
+
+solution = Solution()
+print(solution.findAnagrams(s = "cbaebabacd", p = "abc"))
