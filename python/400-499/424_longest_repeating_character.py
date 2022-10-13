fa42f41cfce43d7a649c6cd5478b38e7f415505b
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-13 11:03:05 +0700

    424_longest_repeating_character

---
+++ python/400-499/424_longest_repeating_character.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    class Solution:
+        def characterReplacement(self, s: str, k: int) -> int:
+            return 5
+
+
+
+
+
+solution = Solution()
+print(solution.characterReplacement(s = "ABAB", k = 2))
