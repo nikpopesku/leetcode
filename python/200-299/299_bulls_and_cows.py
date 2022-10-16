8aa948d6dcbafe20adb72ad258f27542449b64ff
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-16 23:38:22 +0530

    499_bulls_and_cows

---
+++ python/200-299/299_bulls_and_cows.py
@@ -0,0 +1,12 @@
+import sys
+from typing import List, Set
+
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        return '1A3B'
+
+
+
+
+solution = Solution()
+print(solution.getHint(secret = "1807", guess = "7810"))
