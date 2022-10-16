ceb0167964766779e383520496a6aa0d67150d49
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-16 23:11:14 +0530

    424_longest_repeating_character

--- python/400-499/424_longest_repeating_character.py
+++ python/400-499/424_longest_repeating_character.py
@@ -2,7 +2,18 @@ from typing import List
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        return 5
+        d = {}
+        window = 0
+
+        for i, char in enumerate(s):
+            d[char] = (0 if char not in d else d[char]) + 1
+
+            if window + 1 - max(d.values()) <= k:
+                window += 1
+            else:
+                d[s[i - window]] -= 1
+
+        return window
 
 
 
