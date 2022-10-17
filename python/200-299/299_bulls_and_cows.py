3a76c3e7a86e6c9c5470aa8680acf082be59fde6
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-17 16:28:38 +0530

    499_bulls_and_cows

--- python/200-299/299_bulls_and_cows.py
+++ python/200-299/299_bulls_and_cows.py
@@ -3,10 +3,30 @@ from typing import List, Set
 
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
-        return '1A3B'
+        secret_map = {}
+
+        for key, char in enumerate(secret):
+            secret_map[char] = set() if char not in secret_map else secret_map[char]
+            secret_map[char].add(key)
+
+        bulls = 0
+        cows = 0
+
+        for key, char in enumerate(guess):
+            if char in secret_map:
+                if key in secret_map[char]:
+                    bulls += 1
+                    secret_map[char] = secret_map[char] - {key}
+                else:
+                    secret_map[char].pop()
+                    cows += 1
+                if len(secret_map[char]) == 0:
+                    del secret_map[char]
+
+        return str(bulls) + 'A' + str(cows) + 'B'
 
 
 
 
 solution = Solution()
-print(solution.getHint(secret = "1807", guess = "7810"))
+print(solution.getHint(secret = "011", guess = "110"))
