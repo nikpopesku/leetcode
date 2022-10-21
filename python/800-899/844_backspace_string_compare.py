5b37fd68bf0fb257f8ab71d14f67b042e90f3fc2
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-21 21:23:12 +0530

    844_backspace_string_compare

---
+++ python/800-899/844_backspace_string_compare.py
@@ -0,0 +1,10 @@
+import sys
+from typing import Optional
+from math import ceil
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        return True
+
+solution = Solution()
+print(solution.backspaceCompare( s = "ab#c", t = "ad#c"))
