ff8eedc44f2272b059ec1b45509956a66969d89d
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-21 21:31:46 +0530

    844_backspace_string_compare

--- python/800-899/844_backspace_string_compare.py
+++ python/800-899/844_backspace_string_compare.py
@@ -4,7 +4,22 @@ from math import ceil
 
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        return True
+        stack_s = []
+        stack_t = []
+
+        for char in s:
+            if char != '#':
+                stack_s.append(char)
+            elif char == '#' and stack_s:
+                stack_s.pop()
+
+        for char in t:
+            if char != '#':
+                stack_t.append(char)
+            elif char == '#' and stack_t:
+                stack_t.pop()
+
+        return stack_s == stack_t
 
 solution = Solution()
 print(solution.backspaceCompare( s = "ab#c", t = "ad#c"))
