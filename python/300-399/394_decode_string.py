2058e3334cee20497cdb983085a40ad727ddcc99
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-21 21:37:47 +0530

    394_decode_string

---
+++ python/300-399/394_decode_string.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        return 'aaabcbc'
+
+
+solution = Solution()
+print(solution.decodeString(s = "3[a]2[bc]"))
