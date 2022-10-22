efb3ed1593826783da88ad361c3369d2ef53c187
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-22 23:43:50 +0530

    1046_last_stone_weight

--- python/1000-1099/1046_last_stone_weight.py
+++ python/1000-1099/1046_last_stone_weight.py
@@ -1,18 +1,22 @@
-import sys
 from typing import List
+import heapq
 
 
 class Solution:
-    def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        response = 0
-        maxi = 0
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        updated_stones = []
 
-        for k in range(len(values)):
-            response = max(response, maxi + values[k] - k)
-            maxi = max(maxi, values[k] + k)
+        if len(stones) == 1:
+            return stones[0]
 
-        return response
+        for value in stones:
+            heapq.heappush(updated_stones, -1 * value)
+
+        for i in range(len(stones) - 1):
+            heapq.heappush(updated_stones, heapq.heappop(updated_stones) - heapq.heappop(updated_stones))
+
+        return abs(updated_stones[0])
 
 
 solution = Solution()
-print(solution.maxScoreSightseeingPair(values = [1,2]))
+print(solution.lastStoneWeight(stones = [2,7,4,1,8,1]))
