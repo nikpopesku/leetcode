4aec5b6faa479cfed45479fc67f10d1f7fb10411
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-23 22:24:33 +0530

    692_top_k_frequent_words

--- python/600-699/692_top_k_frequent_words.py
+++ python/600-699/692_top_k_frequent_words.py
@@ -4,13 +4,12 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         words = sorted(words)
-        tem = Counter(words).most_common(k)
 
-        return [i[0] for i in tem]
+        return [i[0] for i in Counter(words).most_common(k)]
 
 
 
 
 
 solution = Solution()
-print(solution.topKFrequent(words = ["i","love","leetcode","i","love","coding"], k = 2))
+print(solution.topKFrequent(words = ["i","love","leetcode","i","love","coding"], k = 3))
