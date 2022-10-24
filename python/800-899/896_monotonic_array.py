8bde0b3fc8c9124dbfc1b4fd5f358bb0961b9038
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-24 22:08:44 +0530

    896_monotonic_array

--- python/800-899/896_monotonic_array.py
+++ python/800-899/896_monotonic_array.py
@@ -4,7 +4,16 @@ from math import ceil
 
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        return all(nums[i-1] >= nums[i] for i in range(1, len(nums))) or all(nums[i-1] <= nums[i] for i in range(1, len(nums)))
+        increasing, decreasing = True, True
+
+        for i in range(1, len(nums)):
+            increasing = increasing and nums[i] >= nums[i - 1]
+            decreasing = decreasing and nums[i] <= nums[i - 1]
+
+            if not increasing and not decreasing:
+                return False
+
+        return True
 
 solution = Solution()
 print(solution.isMonotonic(nums = [1,2,2,3]))
