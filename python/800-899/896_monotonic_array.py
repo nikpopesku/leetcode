17fbcb931516d2f36b4e3ba33ae721c9a723a0fb
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-24 21:35:35 +0530

    896_monotonic_array

---
+++ python/800-899/896_monotonic_array.py
@@ -0,0 +1,10 @@
+import sys
+from typing import List
+from math import ceil
+
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        return True
+
+solution = Solution()
+print(solution.isMonotonic(nums = [1,2,2,3]))
