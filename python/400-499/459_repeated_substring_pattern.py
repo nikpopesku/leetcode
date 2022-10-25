b010e820ef4204fcbb4ce1a488cc0ea53f70bc5d
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-25 22:03:42 +0530

    459_repeated_substring_pattern

--- python/400-499/459_repeated_substring_pattern.py
+++ python/400-499/459_repeated_substring_pattern.py
@@ -3,15 +3,7 @@ from typing import List
 
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        array_s = list(s)
-
-        for lenght in range(1, (len(s) // 2) + 1):
-            if len(s) % lenght == 1:
-                continue
-            if array_s[:lenght] * int(len(s) // lenght) == array_s:
-                return True
-
-        return False
+        return s in s[1:] + s[:-1]
 
 
 solution = Solution()
