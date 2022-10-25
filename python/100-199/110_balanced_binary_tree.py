1eca39e91bdff416990b8d1b2b3bc7579da3c226
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-25 20:17:14 +0530

    110_balanced_binary_tree

---
+++ python/100-199/110_balanced_binary_tree.py
@@ -0,0 +1,10 @@
+from typing import Optional, List
+from treenode import TreeNode
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return True
+
+
+solution = Solution()
+print(solution.isBalanced(root = [1,2,2,3,3,null,null,4,4]))
