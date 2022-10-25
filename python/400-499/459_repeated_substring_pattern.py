b13da01c49feac9e8fde096cb48b52302f0e2a51
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-25 20:59:51 +0530

    459_repeated_substring_pattern

---
+++ python/400-499/459_repeated_substring_pattern.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        return True
+
+
+solution = Solution()
+print(solution.repeatedSubstringPattern(s = "abab"))
