945d6d79075f8b074c8ea31e2d7e57195d430a02
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-25 20:02:41 +0530

    28_find_index_of_first_occurence_in_string

--- python/0-99/28_find_index_of_first_occurence_in_string.py
+++ python/0-99/28_find_index_of_first_occurence_in_string.py
@@ -2,21 +2,9 @@ import sys
 from typing import List
 
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        previous = None
-        unique = 0
-
-        for element in nums:
-            if element != previous and previous != None:
-                previous = element
-                nums[unique] = element
-                unique += 1
-            elif previous == None:
-                unique += 1
-                previous = element
-
-        return unique
+    def strStr(self, haystack: str, needle: str) -> int:
+        return 5
 
 
 solution = Solution()
-print(solution.removeDuplicates([int(x) for x in sys.argv[1:]]))
+print(solution.strStr(haystack = "sadbutsad", needle = "sad"))
