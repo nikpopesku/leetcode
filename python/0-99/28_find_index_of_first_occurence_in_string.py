d010f20e975b346674b57bd3ea73f58e45970fd5
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-25 20:10:52 +0530

    28_find_index_of_first_occurence_in_string

--- python/0-99/28_find_index_of_first_occurence_in_string.py
+++ python/0-99/28_find_index_of_first_occurence_in_string.py
@@ -3,7 +3,14 @@ from typing import List
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return 5
+        if len(needle) > len(haystack):
+            return -1
+
+        for key, char in enumerate(haystack):
+            if char == needle[0] and haystack[key:key + len(needle)] == needle:
+                return key
+
+        return -1
 
 
 solution = Solution()
