79484aeeb29bbeabfc06560320c87565f1f8949b
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-28 21:42:06 +0530

    UN-20284: Applied fix

---
+++ python/100-199/150_evaluate_reverse_polish_notation.py
@@ -0,0 +1,11 @@
+
+from typing import Optional, List
+from treenode import TreeNode
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        return 9
+
+
+solution = Solution()
+print(solution.evalRPN(tokens = ["2","1","+","3","*"]))
