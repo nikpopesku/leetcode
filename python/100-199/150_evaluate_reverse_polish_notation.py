1ceaa4f15a05836dcd8122bac8fd7ae37604255c
npopesku <nikolay.popesku@unbiased.co.uk>
2022-10-28 22:22:15 +0530

    150_evaluate_reverse_polish_notation

--- python/100-199/150_evaluate_reverse_polish_notation.py
+++ python/100-199/150_evaluate_reverse_polish_notation.py
@@ -1,11 +1,33 @@
 
 from typing import Optional, List
-from treenode import TreeNode
+import operator
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        return 9
+        operations = {"+", "-", "*", "/"}
+        stack = []
+
+        for token in tokens:
+            if token not in operations:
+                stack.append(token)
+            else:
+                operand2 = int(stack.pop())
+                operand1 = int(stack.pop())
+                match token:
+                    case "+":
+                        element = operator.add(operand1, operand2)
+                    case "-":
+                        element = operator.sub(operand1, operand2)
+                    case "*":
+                        element = operator.mul(operand1, operand2)
+                    case "/":
+                        element = operator.truediv(operand1, operand2)
+                        element = ceil(element) if element < 0 else floor(element)
+
+                stack.append(element)
+
+        return stack.pop()
 
 
 solution = Solution()
-print(solution.evalRPN(tokens = ["2","1","+","3","*"]))
+print(solution.evalRPN(tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
