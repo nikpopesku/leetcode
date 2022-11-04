7951509052d41115777dbec36303138e29fecaed
npopesku <nikolay.popesku@unbiased.co.uk>
2022-11-04 12:03:10 +0530

    1367_linked_list_in_binary_tree

--- python/1300-1399/1367_linked_list_in_binary_tree.py
+++ python/1300-1399/1367_linked_list_in_binary_tree.py
@@ -7,19 +7,20 @@ from treenode import TreeNode
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
         response = False
+        responseLeft = False
+        responseRight = False
 
         if root and head and root.val == head.val:
             response = True
+            head = head.next
 
-        if root.left:
-            xxx = head.next if response else head
-            response = self.isSubPath(xxx, root.left)
+        if root.left and head:
+            responseLeft = self.isSubPath(head, root.left)
 
-        if not response and root.right:
-            xxx = head.next if response else head
-            response = self.isSubPath(xxx, root.right)
+        if root.right and head and not responseLeft:
+            responseRight = self.isSubPath(head, root.right)
 
-        return response
+        return (response and head is None) or (response and (responseLeft or responseRight))
 
 listNode = ListNode(1, ListNode(4, ListNode(2)))
 root = TreeNode(1, TreeNode(4, None, TreeNode(2, TreeNode(1))), TreeNode(4, TreeNode(2, TreeNode(6), TreeNode(8, TreeNode(1), TreeNode(3)))))
