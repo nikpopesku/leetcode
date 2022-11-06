98bddc7d8326f393750b4d0cda96c4af8e064b3a
npopesku <nikolay.popesku@unbiased.co.uk>
2022-11-06 20:13:14 +0530

    989_add_to_array_form_of_integer

--- python/900-999/989_add_to_array_form_of_integer.py
+++ python/900-999/989_add_to_array_form_of_integer.py
@@ -2,23 +2,9 @@ import sys
 from typing import List
 
 class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        length = len(nums)
-        start, end = 0, length - 1
-        response = [0] * length
-        idx = end
-
-        while start <= end:
-            if abs(nums[end]) > abs(nums[start]):
-                response[idx] = nums[end] * nums[end]
-                end -= 1
-            else:
-                response[idx] = nums[start] * nums[start]
-                start += 1
-            idx -= 1
-
-        return response
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        return [int(c) for c in list(str(int(''.join(str(c) for c in num)) + k))]
 
 
 solution = Solution()
-print(solution.sortedSquares([int(x) for x in sys.argv[1:]]))
+print(solution.addToArrayForm(num = [1,2,0,0], k = 34))
