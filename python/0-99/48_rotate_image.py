44a02c0f9a7546e35836b9df4ff04b1327f92e0a
npopesku <nikolay.popesku@unbiased.co.uk>
2022-11-07 22:51:46 +0530

    48_rotate_image

--- python/0-99/48_rotate_image.py
+++ python/0-99/48_rotate_image.py
@@ -10,13 +10,11 @@ class Solution:
         l,k = 0, len(matrix)
 
         while l < k:
-            submatrix = matrix[l:k]
-
-            for i in range(len(submatrix) - 1):
-                submatrix[i][len(submatrix) - 1], temp = submatrix[0][i], submatrix[i][len(submatrix) - 1]
-                submatrix[len(submatrix) - 1][len(submatrix) - i - 1], temp = temp, submatrix[len(submatrix) - 1][len(submatrix) - i - 1]
-                submatrix[len(submatrix) - i - 1][0], temp = temp, submatrix[len(submatrix) - i - 1][0]
-                submatrix[0][i] = temp
+            for i in range(l, k - 1):
+                matrix[i][len(matrix) - 1], temp = matrix[0][i], matrix[i][len(matrix) - 1]
+                matrix[len(matrix) - 1][len(matrix) - i - 1], temp = temp, matrix[len(matrix) - 1][len(matrix) - i - 1]
+                matrix[len(matrix) - i - 1][0], temp = temp, matrix[len(matrix) - i - 1][0]
+                matrix[0][i] = temp
 
             l = l + 1
             k = k - 1
@@ -24,4 +22,4 @@ class Solution:
 
 
 solution = Solution()
-print(solution.rotate(matrix = [[1,2,3],[4,5,6],[7,8,9]]))
+print(solution.rotate(matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
