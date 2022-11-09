7f54f33ed11afec666a1051f4f4596a1023db587
npopesku <nikolay.popesku@unbiased.co.uk>
2022-11-09 19:59:34 +0530

    54_spiral_matrix

--- python/0-99/54_spiral_matrix.py
+++ python/0-99/54_spiral_matrix.py
@@ -3,22 +3,7 @@ from typing import List, Set
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        height_l, height_k = 0, len(matrix)
-        width_l, width_k = 0, len(matrix[0])
-
-        response = []
-
-        while width_l < width_k and height_l < height_k:
-            response = response + matrix[height_l][width_l:width_k]
-            response = response + [matrix[i][width_k - 1] for i in range(height_l+1, height_k - 1)]
-            response = response + matrix[height_k - 1][width_l:width_k:-1]
-            response = response + [matrix[i][width_l] for i in range(height_k-1, height_l - 1, -1)]
-            width_l = width_l + 1
-            width_k = width_k - 1
-            height_l = height_l + 1
-            height_k = height_k - 1
-
-        return response
+        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
 
 
 
