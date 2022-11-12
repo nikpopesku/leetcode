ed8f1278a266c6377b8efdea2002eef4cfbcaeff
npopesku <nikolay.popesku@unbiased.co.uk>
2022-11-12 15:13:30 +0530

    503_next_greater_element_2

--- python/500-599/503_next_greater_element_2.py
+++ python/500-599/503_next_greater_element_2.py
@@ -4,7 +4,15 @@ from typing import List
 
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        return [next(x for x in nums[i+1:] + nums[:i] if x > nums[i]) for i in range(len(nums))]
+        response = []
+
+        for i in range(len(nums)):
+            try:
+                response.append(next(x for x in nums[i + 1:] + nums[:i] if x > nums[i]))
+            except StopIteration:
+                response.append(-1)
+
+        return response
 
 
 solution = Solution()
