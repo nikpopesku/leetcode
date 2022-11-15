4e258df4ef75750a7ebd40d956418171a76cbb67
npopesku <nikolay.popesku@unbiased.co.uk>
2022-11-15 09:59:48 +0530

    1376_time_to_inform_all_employees

--- python/1300-1399/1376_time_to_inform_all_employees.py
+++ python/1300-1399/1376_time_to_inform_all_employees.py
@@ -6,21 +6,24 @@ class Solution:
         if n == 1:
             return 0
 
-        before_last = last = (headID,)
+        queue = [headID]
         manager_map = {i: manager[i] for i in range(len(manager))}
         max_time = 0
-        time = (0,)
-        times = ()
+        times = {headID: 0}
 
-        while last:
-            last = tuple(k for k,v in manager_map.items() if v in before_last)
-            if last:
-                time = tuple(informTime[manager_map[i]] + time[before_last.index(manager_map[i])] for i in last)
-                before_last = last
-                times = times + time
+        while queue:
+            element = queue.pop(0)
+            temp = []
+            for k, v in manager_map.items():
+                if v == element:
+                    temp.append(k)
+                    queue.append(k)
+                    times[k] = informTime[v] + times[v]
+            for l in temp:
+                del manager_map[l]
 
 
-        return max(max_time, *times)
+        return max(max_time, *times.values())
 
 solution = Solution()
 print(solution.numOfMinutes(n = 6, headID = 2, manager = [2,2,-1,2,2,2], informTime = [0,0,1,0,0,0]))
