8928699224b6e04535886afe684b9950cd339e77
npopesku <nikolay.popesku@unbiased.co.uk>
2022-11-28 09:43:47 +0530

    2131_longest_palindrome_by_concat_two_letters

--- python/2100-2199/2131_longest_palindrome_by_concat_two_letters.py
+++ python/2100-2199/2131_longest_palindrome_by_concat_two_letters.py
@@ -1,5 +1,25 @@
 from typing import List
+import collections
 
 class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
-        return list(dict((item, nums[item]) for index, item in nums).values())
+    def longestPalindrome(self, words: List[str]) -> int:
+        collection_words = collections.Counter(words)
+        counter = 0
+
+        while collection_words:
+            k = list(collection_words.keys())[0]
+
+            if k[0] == k[1]:
+                counter = counter + collection_words[k] * 2
+            else:
+                v2 = collection_words[k[::-1]] if k[::-1] in collection_words else 0
+                counter = counter + min(collection_words[k], v2) * 2
+                if v2:
+                    del collection_words[k[::-1]]
+
+            del collection_words[k]
+
+        return counter
+
+solution = Solution()
+print(solution.longestPalindrome(words = ["lc","cl","gg"]))
