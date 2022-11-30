1430b2e579086481e2550657cbc1bff8515757fd
npopesku <nikolay.popesku@unbiased.co.uk>
2022-11-30 21:16:10 +0530

    113_path_sum_2

--- python/100-199/113_path_sum_2.py
+++ python/100-199/113_path_sum_2.py
@@ -1,5 +1,6 @@
 from typing import Optional, List
 from treenode import TreeNode
+import copy
 
 class Solution:
     response = []
@@ -11,18 +12,20 @@ class Solution:
         while stack:
             element, path = stack.pop()
 
-            if element.val == targetSum and not element.left and not element.right:
-                path[:].append(element.val)
+            if element.val + sum(path) == targetSum and not element.left and not element.right:
+                path.append(element.val)
                 response.append(path)
             if element.left:
-                path[:].append(element.val)
-                stack.append((element.left, path))
+                temp = path[:]
+                temp.append(element.val)
+                stack.append((element.left, temp))
             if element.right:
-                path[:].append(element.val)
-                stack.append((element.right, path))
+                temp = path[:]
+                temp.append(element.val)
+                stack.append((element.right, temp))
 
         return response
 
 r1 = TreeNode(-2, None, TreeNode(-3))
 solution = Solution()
-print(solution.hasPathSum(root = TreeNode(5, TreeNode(4, TreeNode(11, TreeNode(7), TreeNode(2))), TreeNode(8, TreeNode(13), TreeNode(4, TreeNode(5), TreeNode(1))))))
+print(solution.pathSum(root = TreeNode(5, TreeNode(4, TreeNode(11, TreeNode(7), TreeNode(2))), TreeNode(8, TreeNode(13), TreeNode(4, TreeNode(5), TreeNode(1)))), targetSum=22))
