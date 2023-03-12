13ca1f0824aaa797d88d19278fe308d93d690440
npopesku <nikolay.popesku@unbiased.co.uk>
2023-03-12 13:51:12 +0200

    254_factor_combinations.py

--- python/200-299/254_factor_combinations.py
+++ python/200-299/254_factor_combinations.py
@@ -1,35 +1,20 @@
 from typing import List
-from bisect import bisect_left
 
 class Solution:
     def getFactors(self, n: int):
         if n == 1:
             return []
 
-        results = set()
+        results = []
 
-        def backtrack(target: int, limit: int, combination: List[int]):
-            if target == 1 and tuple(combination) not in results:
-                    results.add(tuple(combination))
+        def calc(target: int, start: int, combination: List[int]):
+            while start * start <= target:
+                if target % start == 0:
+                    results.append(combination + [start, target // start])
+                    calc(target // start, start, combination + [start])
+                start += 1
 
-            for i in range(2, limit + 1):
-                if target % i == 0:
-                    div_res = int(target / i)
-                    pos = bisect_left(combination, i)
-                    new_comb = combination[:pos] + [i] + combination[pos:]
-                    exists = False
-
-                    for c in results:
-                        if c[:len(new_comb)] == tuple(new_comb):
-                            exists = True
-                            break
-
-                    if not exists:
-                        backtrack(div_res, div_res, new_comb)
-
-            return results
-
-        backtrack(n, n // 2, [])
+        calc(n, 2, [])
 
         return results
 
