c864273921770a110171a3dd1f1981b51951361d
npopesku <nikolay.popesku@unbiased.co.uk>
2023-03-14 10:54:06 +0200

    329_longest_increasing_path.py

---
+++ python/300-399/329_longest_increasing_path.py
@@ -0,0 +1,18 @@
+import sys
+from typing import List
+
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        matrix = {i + j * 1j: val
+                  for i, row in enumerate(matrix)
+                  for j, val in enumerate(row)}
+        length = {}
+        for z in sorted(matrix, key=matrix.get):
+            length[z] = 1 + max([length[Z]
+                                 for Z in [1, -1, 1j, -1j]
+                                 if z + Z in matrix and matrix[z] > matrix[z+Z]]
+                                or [0])
+        return max(length.values() or [0])
+
+solution = Solution()
+print(solution.longestIncreasingPath(list(sys.argv[1])))
