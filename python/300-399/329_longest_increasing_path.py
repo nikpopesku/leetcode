ea9923983b5d0f92809f31b43567a33e9dc1adb1
npopesku <nikolay.popesku@unbiased.co.uk>
2023-03-18 11:19:15 +0200

    329_longest_increasing_path.py

--- python/300-399/329_longest_increasing_path.py
+++ python/300-399/329_longest_increasing_path.py
@@ -7,10 +7,7 @@ class Solution:
                   for j, val in enumerate(row)}
         length = {}
         for z in sorted(matrix, key=matrix.get):
-            length[z] = 1 + max([length[Z]
-                                 for Z in [1, -1, 1j, -1j]
-                                 if z + Z in matrix and matrix[z] > matrix[z+Z]]
-                                or [0])
+            length[z] = 1 + max([length[Z] for Z in [z + 1, z - 1, z + 1j, z -1j] if Z in matrix and matrix[z] > matrix[Z]] or [0])
         return max(length.values() or [0])
 
 solution = Solution()
