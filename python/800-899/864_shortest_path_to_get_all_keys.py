a4810a19e74ddb56017f48193dfeb7c8010efad9
npopesku <nikolay.popesku@unbiased.co.uk>
2024-09-19 19:41:20 +0800

    python/800-899/864_shortest_path_to_get_all_keys.py

--- python/800-899/864_shortest_path_to_get_all_keys.py
+++ python/800-899/864_shortest_path_to_get_all_keys.py
@@ -2,51 +2,40 @@ from typing import List
 
 
 class Solution:
-    def __init__(self):
-        self.grid = None
-
     def shortestPathAllKeys(self, grid: List[str]) -> int:
-        self.grid = grid
         m, n = len(grid), len(grid[0])
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        stack = self.start(m, n)
-        response = 0
         numberKeys = 0
-        keys = set()
-        visited = set(stack)
+        stack = []
+        minSteps = float('inf')
 
         for row in range(m):
             for col in range(n):
                 if grid[row][col].isalnum() and grid[row][col].islower():
                     numberKeys += 1
+                if grid[row][col] == '@':
+                    stack = [(row, col, 0, '', {(row, col)})]
 
         while stack:
-            newStack = set()
-            for x, y in stack:
-                for dx, dy in directions:
-                    nx, ny = x + dx, y + dy
-                    if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] != '#' and (nx, ny) not in visited:
-                        visited.add((nx, ny))
-                        if grid[nx][ny].isupper() and grid[nx][ny].lower() not in keys:
-                            continue
-                        if grid[nx][ny].islower() and grid[nx][ny] not in keys:
-                            keys.add(grid[nx][ny])
-                            visited = {(nx, ny)}
-                        newStack.add((nx, ny))
-            stack = newStack
-            response += 1
-            if len(keys) == numberKeys:
-                break
+            x, y, steps, keys, visited = stack.pop()
+            if steps >= minSteps:
+                continue
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] != '#' and (nx, ny) not in visited:
+                    if grid[nx][ny].isupper() and grid[nx][ny].lower() not in keys:
+                        continue
+                    if grid[nx][ny].islower() and grid[nx][ny] not in keys:
+                        stack.append((nx, ny, steps + 1, keys + grid[nx][ny], {(nx, ny)}))
+                        if len(keys + grid[nx][ny]) == numberKeys:
+                            minSteps = min(minSteps, steps + 1)
+                    else:
+                        stack.append((nx, ny, steps + 1, str(keys), visited | {(nx, ny)}))
 
-        return response if len(keys) == numberKeys else -1
+        return minSteps if minSteps < float('inf') else -1
 
-    def start(self, m: int, n: int):
-        for row in range(m):
-            for col in range(n):
-                if self.grid[row][col] == '@':
-                    return {(row, col)}
+
 
 solution = Solution()
-grid = ["@...a",".###A","b.BCc"]
+grid = ["###.D.B.F.","..#b......","##....#..#","....@..#..","#d.AE.##c#",".....##.f.","#........C","..a#.#....","#....#e..#","........##"]
 print(solution.shortestPathAllKeys(grid))
