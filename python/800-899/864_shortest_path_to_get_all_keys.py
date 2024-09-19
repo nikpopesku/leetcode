604f5fa49e4773d7cbbf9fab629f6ff2de78a934
npopesku <nikolay.popesku@unbiased.co.uk>
2024-09-19 18:22:02 +0800

    python/800-899/864_shortest_path_to_get_all_keys.py

--- python/800-899/864_shortest_path_to_get_all_keys.py
+++ python/800-899/864_shortest_path_to_get_all_keys.py
@@ -9,15 +9,20 @@ class Solution:
         self.grid = grid
         m, n = len(grid), len(grid[0])
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        start = None
 
         stack = self.start(m, n)
         response = 0
+        numberKeys = 0
         keys = set()
         visited = set(stack)
 
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col].isalnum() and grid[row][col].islower():
+                    numberKeys += 1
+
         while stack:
-            newStack = []
+            newStack = set()
             for x, y in stack:
                 for dx, dy in directions:
                     nx, ny = x + dx, y + dy
@@ -25,21 +30,23 @@ class Solution:
                         visited.add((nx, ny))
                         if grid[nx][ny].isupper() and grid[nx][ny].lower() not in keys:
                             continue
-                        if grid[nx][ny].islower():
-                            keys.add((nx, ny))
-                            visited = {nx, ny}
-                        newStack.append((nx, ny))
+                        if grid[nx][ny].islower() and grid[nx][ny] not in keys:
+                            keys.add(grid[nx][ny])
+                            visited = {(nx, ny)}
+                        newStack.add((nx, ny))
             stack = newStack
             response += 1
+            if len(keys) == numberKeys:
+                break
 
-        return response
+        return response if len(keys) == numberKeys else -1
 
     def start(self, m: int, n: int):
         for row in range(m):
             for col in range(n):
                 if self.grid[row][col] == '@':
-                    return [(row, col)]
+                    return {(row, col)}
 
 solution = Solution()
-grid = ["@.a..","###.#","b.A.B"]
+grid = ["@...a",".###A","b.BCc"]
 print(solution.shortestPathAllKeys(grid))
