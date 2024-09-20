5a39f336f1d5129fb67d45f174de22d2ad907abf
npopesku <nikolay.popesku@unbiased.co.uk>
2024-09-20 16:09:48 +0800

    python/800-899/864_shortest_path_to_get_all_keys.py

--- python/800-899/864_shortest_path_to_get_all_keys.py
+++ python/800-899/864_shortest_path_to_get_all_keys.py
@@ -1,38 +1,54 @@
 from typing import List
+from itertools import permutations
+from collections import deque
 
 
 class Solution:
     def shortestPathAllKeys(self, grid: List[str]) -> int:
-        m, n = len(grid), len(grid[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        numberKeys = 0
-        stack = []
-        minSteps = float('inf')
+        R, C = len(grid), len(grid[0])
+        # location['a'] = the coordinates of 'a' on the grid, etc.
+        location = {v: (r, c)
+                    for r, row in enumerate(grid)
+                    for c, v in enumerate(row)
+                    if v not in '.#'}
 
-        for row in range(m):
-            for col in range(n):
-                if grid[row][col].isalnum() and grid[row][col].islower():
-                    numberKeys += 1
-                if grid[row][col] == '@':
-                    stack = [(row, col, 0, '', {(row, col)})]
+        def neighbors(r, c):
+            for cr, cc in ((r-1, c), (r, c-1), (r+1, c), (r, c+1)):
+                if 0 <= cr < R and 0 <= cc < C:
+                    yield cr, cc
 
-        while stack:
-            x, y, steps, keys, visited = stack.pop()
-            if steps >= minSteps:
-                continue
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] != '#' and (nx, ny) not in visited:
-                    if grid[nx][ny].isupper() and grid[nx][ny].lower() not in keys:
-                        continue
-                    if grid[nx][ny].islower() and grid[nx][ny] not in keys:
-                        stack.append((nx, ny, steps + 1, keys + grid[nx][ny], {(nx, ny)}))
-                        if len(keys + grid[nx][ny]) == numberKeys:
-                            minSteps = min(minSteps, steps + 1)
-                    else:
-                        stack.append((nx, ny, steps + 1, str(keys), visited | {(nx, ny)}))
+        def bfs(source, target, keys = ()):
+            sr, sc = location[source]
+            tr, tc = location[target]
+            seen = [[False] * C for _ in range(R)]
+            seen[sr][sc] = True
+            queue = deque([(sr, sc, 0)])
+            while queue:
+                r, c, d = queue.popleft()
+                if r == tr and c == tc: return d
+                for cr, cc in neighbors(r, c):
+                    if not seen[cr][cc] and grid[cr][cc] != '#':
+                        if grid[cr][cc].isupper() and grid[cr][cc].lower() not in keys:
+                            continue
+                        queue.append((cr,cc,d+1))
+                        seen[cr][cc] = True
+            return float('inf')
 
-        return minSteps if minSteps < float('inf') else -1
+        ans = float('inf')
+        keys = "".join(chr(ord('a') + i) for i in range(len(location) // 2))
+        for cand in permutations(keys):
+            # bns : the built candidate answer, consisting of the sum
+            # of distances of the segments from '@' to cand[0] to cand[1] etc.
+            bns = 0
+            for i, target in enumerate(cand):
+                source = cand[i-1] if i > 0 else '@'
+                d = bfs(source, target, cand[:i])
+                bns += d
+                if bns >= ans: break
+            else:
+                ans = bns
+
+        return ans if ans < float('inf') else -1
 
 
 
