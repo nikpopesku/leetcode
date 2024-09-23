74ef83a3b0000ca320e013efa13e6f3554b59b22
npopesku <nikolay.popesku@unbiased.co.uk>
2024-09-23 13:30:22 +0800

    python/1100-1199/1135_connecting_cities_with_minimum.py

--- python/1100-1199/1135_connecting_cities_with_minimum.py
+++ python/1100-1199/1135_connecting_cities_with_minimum.py
@@ -4,21 +4,27 @@ from collections import defaultdict
 class Solution:
     def minimumCost(self, n: int, connections: List[List[int]]) -> int:
         connections.sort(key=lambda x: x[-1])
-        cost = 0
+        totalCost = 0
+        totalEdges = 0
         counter = 0
         mst = defaultdict(list)
 
 
-        while len(mst) < 2 * (len(connections) - 1):
-            start, end, c = connections[counter]
+        while totalEdges < n - 1:
+            if counter > len(connections) - 1:
+                break
+            start, end, cost = connections[counter]
             prev, cur = None, start
 
             while cur in mst and cur != end:
                 changed = False
                 for nxt in mst[cur]:
                     if nxt != prev:
-                        cur, last = nxt, cur
+                        prev = cur
+                        cur = nxt
                         changed = True
+                    if changed:
+                        break
 
                 if not changed:
                     break
@@ -26,12 +32,13 @@ class Solution:
             if cur == end:
                 continue
 
-            cost += c
+            totalCost += cost
+            totalEdges += 1
             mst[start].append(end)
             mst[end].append(start)
             counter += 1
 
-        return cost
+        return totalCost if totalEdges + 1 == n else -1
 
 solution = Solution()
 print(solution.minimumCost(n = 3, connections = [[1,2,5],[1,3,6],[2,3,1]]))
