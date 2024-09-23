3785888f523c5900b00a94beeab29dac3a0254a3
npopesku <nikolay.popesku@unbiased.co.uk>
2024-09-23 15:33:59 +0800

    python/1100-1199/1135_connecting_cities_with_minimum.py

--- python/1100-1199/1135_connecting_cities_with_minimum.py
+++ python/1100-1199/1135_connecting_cities_with_minimum.py
@@ -1,44 +1,50 @@
 from typing import List
 from collections import defaultdict
 
+
 class Solution:
+    def __init__(self):
+        self.rank = []
+        self.root = []
+
     def minimumCost(self, n: int, connections: List[List[int]]) -> int:
         connections.sort(key=lambda x: x[-1])
         totalCost = 0
-        totalEdges = 0
-        counter = 0
-        mst = defaultdict(list)
+        totalConnections = 0
+        self.rank = [1 for i in range(1, n + 1)]
+        self.root = [i for i in range(1, n + 1)]
 
-
-        while totalEdges < n - 1:
-            if counter > len(connections) - 1:
-                break
-            start, end, cost = connections[counter]
-            prev, cur = None, start
-
-            while cur in mst and cur != end:
-                changed = False
-                for nxt in mst[cur]:
-                    if nxt != prev:
-                        prev = cur
-                        cur = nxt
-                        changed = True
-                    if changed:
-                        break
-
-                if not changed:
-                    break
-
-            if cur == end:
+        for a, b, cost in connections:
+            if (self.root[a] == self.root[b]):
                 continue
-
             totalCost += cost
-            totalEdges += 1
-            mst[start].append(end)
-            mst[end].append(start)
-            counter += 1
+            self.union(a, b)
+            totalConnections += 1
 
-        return totalCost if totalEdges + 1 == n else -1
+        return totalCost if len(connections) - 1 == totalConnections else -1
+
+        return totalCost
+
+    def union(a: int, b: int):
+        rootA = self.root[a]
+        rootB = self.root[b]
+
+        if rootA != rootB:
+            if self.rank[a] > self.rank[b]:
+                self.root[rootB] = rootA
+                self.rank[a] += 1
+            else:
+                self.root[rootA] = rootB
+                self.rank[b] += 1
+
+    def find(a: int):
+        while a != self.root[a]:
+            self.root[a] = self.root[self.root[a]]
+
+            a = self.root[a]
+
+        return a
+
 
 solution = Solution()
 print(solution.minimumCost(n = 3, connections = [[1,2,5],[1,3,6],[2,3,1]]))
