0084c207c97f87e6c55da77046c2ff2975ab95e3
npopesku <nikolay.popesku@unbiased.co.uk>
2024-10-16 14:03:29 +0800

    python/1800-1899/1857_largest_color_value.py

--- python/1800-1899/1857_largest_color_value.py
+++ python/1800-1899/1857_largest_color_value.py
@@ -40,4 +40,4 @@ class Solution:
 
 
 solution = Solution()
-print(solution.largestPathValue(colors = "hhqhuqhqff", edges = [[0,1],[0,2],[2,3],[3,4],[3,5],[5,6],[2,7],[6,7],[7,8],[3,8],[5,8],[8,9],[3,9],[6,9]]))
+print(solution.largestPathValue(colors = "nnllnzznn", edges = [[0,1],[1,2],[2,3],[2,4],[3,5],[4,6],[3,6],[5,6],[6,7],[7,8]]))
