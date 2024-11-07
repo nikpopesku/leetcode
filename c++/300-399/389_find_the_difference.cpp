10daa2bfbef1326970d3767e723d365eaf2d611f
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-07 16:22:11 +0800

    c++/300-399/389_find_the_difference.cpp

---
+++ c++/300-399/389_find_the_difference.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+
+    }
+};
+
+
+int main() {
+    auto s = Solution();
+    cout << s.findTheDifference("abc", "pqr") << endl;
+}
