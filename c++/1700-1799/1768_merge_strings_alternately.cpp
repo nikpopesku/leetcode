86948ddb23dc2f6cb75fae951b77dc337d8a1526
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-07 16:04:37 +0800

    c++/1700-1799/1768_merge_strings_alternately.cpp

--- c++/1700-1799/1768_merge_strings_alternately.cpp
+++ c++/1700-1799/1768_merge_strings_alternately.cpp
@@ -8,7 +8,20 @@ public:
     Solution () {};
 
     string mergeAlternately(string word1, string word2) {
-      return "aadsf";
+        int i {0}, j{0};
+
+        while (i < word1.length() and j < word2.length()) {
+            if (i < word1.length()) {
+                cout << word1[i];
+                i++;
+            }
+
+            if (j < word2.length()) {
+                cout << word2[j];
+                j++;
+            }
+        }
+
     }
 };
 
@@ -18,5 +31,5 @@ int main()
 
 
     auto s = Solution();
-    cout << s.mergeAlternately("a   b   c   d", "  p   q");
+    s.mergeAlternately("abc", "pqr");
 }
