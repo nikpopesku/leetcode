92a3ccd5cb5d15f74e156c9c97e5e8eba32d0ce8
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-07 16:33:49 +0800

    c++/300-399/389_find_the_difference.cpp

--- c++/300-399/389_find_the_difference.cpp
+++ c++/300-399/389_find_the_difference.cpp
@@ -1,17 +1,38 @@
 #include <string>
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        return *"a";
+        unordered_map<char, int> map {};
+
+        for (size_t i = 0; i < s.length(); i++) {
+            if (map.find(s[i]) == map.end()) {
+                map[s[i]] = 0;
+            }
+
+            map[s[i]]++;
+        }
+
+        for (size_t i = 0; i < t.length(); i++) {
+            if (map.find(t[i]) != map.end()) {
+                map[t[i]]--;
+            }
+
+            if (map[t[i]] == 0) {
+                map.erase(t[i]);
+            }
+        }
+
+        return map.begin()->first;
     }
 };
 
 
 int main() {
     auto s = Solution();
-    cout << s.findTheDifference("abc", "pqr") << endl;
+    cout << s.findTheDifference("abcd", "abcde") << endl;
 }
