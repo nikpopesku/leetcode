774a3e980d76c3aac17e50ea98e68b9581cc52b8
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-08 12:32:28 +0800

    c++/900-999/983_minimum_cost_for_tickets.cpp

--- c++/900-999/983_minimum_cost_for_tickets.cpp
+++ c++/900-999/983_minimum_cost_for_tickets.cpp
@@ -5,25 +5,16 @@ using namespace std;
 
 class Solution {
 public:
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
+    int mincostTickets(vector<int> &days, vector<int> &costs) {
         return calc(days, costs, 0);
     }
 
-    int calc(vector<int>& days, vector<int>& costs, int day) {
-        if (days.empty()) return 0;
+    int calc(vector<int> &days, vector<int> &costs, int day) {
+        if (day > days.back()) return 0;
 
         int one = costs[0] + calc(days, costs, day + 1);
-        int seven = costs[1];
-        int thirty = costs[2];
-
-
-        if (days.size() >= 7) {
-            seven += calc(days, costs, day + 7);
-        }
-
-        if (days.size() >= 30) {
-            thirty += calc(days, costs, day + 30);
-        }
+        int seven = costs[1] + calc(days, costs, day + 7);
+        int thirty = costs[2] + calc(days, costs, day + 30);
 
         return min(min(one, seven), thirty);
     }
@@ -31,7 +22,7 @@ public:
 
 int main() {
     auto s = Solution();
-    vector a {1,4,6,7,8,20};
-    vector b {2,7,15};
+    vector a{1, 4, 6, 7, 8, 20};
+    vector b{2, 7, 15};
     cout << s.mincostTickets(a, b) << endl;
 }
