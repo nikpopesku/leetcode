07a595d74dc126538f50d3d47291e8b07c3e22e8
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-08 11:53:41 +0800

    c++/900-999/983_minimum_cost_for_tickets.cpp

---
+++ c++/900-999/983_minimum_cost_for_tickets.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+    cout << s.mincostTickets(vector<int>{1,4,6,7,8,20}, vector<int>{2,7,15}) << endl;
+}
