67355798692511648d62a8f026257adb4926543f
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-09 14:28:02 +0800

    c++/100-199/115_distinct_subsequences.cpp

---
+++ c++/100-199/115_distinct_subsequences.cpp
@@ -0,0 +1,19 @@
+
+#include <string>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        return 5
+    }
+};
+
+
+int main() {
+    auto s = Solution();
+    cout << s.numDistinct("rabbbit", "rabbit") << endl;
+}
