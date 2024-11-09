7aad9f2bb06f6ab79309c035db46215fe0d4c36d
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-09 15:08:26 +0800

    c++/100-199/115_distinct_subsequences.cpp

--- c++/100-199/115_distinct_subsequences.cpp
+++ c++/100-199/115_distinct_subsequences.cpp
@@ -11,16 +11,16 @@ public:
         dp.resize(s.size() + 1, std::vector(t.size(), 0));
 
         dp[0][0] = 1;
-        for (int i = 1; i <= s.size(); i++) dp[i][0] = 1;
-        for (int j = 1; j < t.size(); j++) dp[0][j] = 1;
+        for (int i = 1; i <= s.size(); i++) dp[i][0] = 0;
+        for (int j = 1; j < t.size(); j++) dp[0][j] = 0;
 
         for (auto i = 1; i <= s.size(); i++) {
             for (size_t j = 1; j < t.size(); j++) {
                 int xxx = 0;
                 if (s[i-1] == t[j]) {
-                    xxx = dp[i-1][j-1];
+                    xxx = 1;
                 }
-                dp[i][j] = dp[i - 1][j] + dp[i][j-1] + xxx;
+                dp[i][j] = dp[i - 1][j] + dp[i][j-1] + dp[i-1][j-1] + xxx;
             }
         }
 
