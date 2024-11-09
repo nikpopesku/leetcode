1855e2b5b89daefab003a0fee65fe63d7e193e6e
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-09 14:32:19 +0800

    c++/100-199/115_distinct_subsequences.cpp

--- c++/100-199/115_distinct_subsequences.cpp
+++ c++/100-199/115_distinct_subsequences.cpp
@@ -1,14 +1,24 @@
-
 #include <string>
 #include <iostream>
-#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        return 5;
+        vector<int> dp (s.size() + 1, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 0; j < t.size(); j++) {
+                if (s[i - 1] == t[j]) {
+                    dp[i] += dp[i - 1];
+                }
+            }
+        }
+
+        return dp[s.size()];
     }
 };
 
