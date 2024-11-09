e393592833afba4b8c43d8c43b5f3cda44cbf641
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-09 15:02:59 +0800

    c++/100-199/115_distinct_subsequences.cpp

--- c++/100-199/115_distinct_subsequences.cpp
+++ c++/100-199/115_distinct_subsequences.cpp
@@ -9,17 +9,19 @@ public:
     int numDistinct(const string &s, const string &t) {
         std::vector<std::vector<int>> dp;
         dp.resize(s.size() + 1, std::vector(t.size(), 0));
+
         dp[0][0] = 1;
-
-        for (int i = 1; i <= s.size(); i++) {
-            dp[i][0] = 1;
-        }
-
+        for (int i = 1; i <= s.size(); i++) dp[i][0] = 1;
         for (int j = 1; j < t.size(); j++) dp[0][j] = 1;
 
-        for (size_t i = 1; i <= s.size(); i++) {
-            for (size_t j = 1; j < t.size(); j++)
-                dp[i][j] = dp[i - 1][j];
+        for (auto i = 1; i <= s.size(); i++) {
+            for (size_t j = 1; j < t.size(); j++) {
+                int xxx = 0;
+                if (s[i-1] == t[j]) {
+                    xxx = dp[i-1][j-1];
+                }
+                dp[i][j] = dp[i - 1][j] + dp[i][j-1] + xxx;
+            }
         }
 
         return dp[s.size()][t.size()-1];
