a464245cc269ce706e9f569cdb17b546915a3a52
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-09 15:18:33 +0800

    c++/100-199/115_distinct_subsequences.cpp

--- c++/100-199/115_distinct_subsequences.cpp
+++ c++/100-199/115_distinct_subsequences.cpp
@@ -8,28 +8,28 @@ class Solution {
 public:
     int numDistinct(const string &s, const string &t) {
         std::vector<std::vector<int>> dp;
-        dp.resize(s.size() + 1, std::vector(t.size(), 0));
+        dp.resize(s.size() + 1, std::vector(t.size() + 1, 0));
 
         dp[0][0] = 1;
         for (int i = 1; i <= s.size(); i++) dp[i][0] = 0;
-        for (int j = 1; j < t.size(); j++) dp[0][j] = 0;
+        for (int j = 1; j <= t.size(); j++) dp[0][j] = 0;
 
         for (auto i = 1; i <= s.size(); i++) {
-            for (size_t j = 1; j < t.size(); j++) {
-                int xxx = 0;
-                if (s[i-1] == t[j]) {
-                    xxx = 1;
+            for (size_t j = 1; j <= t.size(); j++) {
+                if (s[i-1] == t[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j-1];
                 }
-                dp[i][j] = dp[i - 1][j] + dp[i][j-1] + dp[i-1][j-1] + xxx;
             }
         }
 
-        return dp[s.size()][t.size()-1];
+        return dp[s.size()][t.size()];
     }
 };
 
 
 int main() {
     auto s = Solution();
-    cout << s.numDistinct("rabbbit", "rabbit") << endl;
+    cout << s.numDistinct("rab", "rab") << endl;
 }
