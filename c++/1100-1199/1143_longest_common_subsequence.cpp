1541b020077e763d72e8638a5832203510e4fbe6
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-10 11:53:12 +0800

    c++/1100-1199/1143_longest_common_subsequence.cpp

--- c++/1100-1199/1143_longest_common_subsequence.cpp
+++ c++/1100-1199/1143_longest_common_subsequence.cpp
@@ -1,18 +1,34 @@
-#include <vector>
 #include <string>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        return 0;
+    string longestCommonSubsequence(string text1, string text2) {
+        vector<size_t> dp (text1.size()+1, vector<size_t>(text2.size()+1, 0));
+    }
+private:
+    string calc(string s1, string s2) {
+        if (s1.empty() or s2.empty()) return '';
+
+
+        if (s1[0] != s2[0]) {
+            auto r1 = calc(s1.substr(1, s1.size() - 1), s2);
+            auto r2 = calc(s1, s2.substr(1, s2.size() - 1));
+
+            if (r1.size() > r2.size()) {
+                return r1;
+            }
+
+            return r2;
+        }
     }
 };
 
 
 int main() {
     auto s = Solution();
-    cout << s.longestCommonSubsequence("rabbbit", "rabbit") << endl;
+    cout << s.longestCommonSubsequence("abcde", "ace") << endl;
 }
