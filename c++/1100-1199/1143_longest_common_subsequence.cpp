c3a6508ea4c966dc64cc184b8b70917b5dd8f339
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-10 12:14:31 +0800

    c++/1100-1199/1143_longest_common_subsequence.cpp

--- c++/1100-1199/1143_longest_common_subsequence.cpp
+++ c++/1100-1199/1143_longest_common_subsequence.cpp
@@ -8,29 +8,29 @@ using namespace std;
 class Solution {
 public:
     string longestCommonSubsequence(const string& text1, const string& text2) {
-        unordered_map<string, string> dp {};
-
-        return calc(text1, text2, dp);
+        return calc(text1, text2);
     }
 private:
-    string calc(const string &s1, const string &s2, unordered_map<string, string>& dp) {
+    string calc(const string &s1, const string &s2) {
         static const string empty;
         if (s1.empty() or s2.empty()) return empty;
         string key = to_string(s1.size()) + "_" + to_string(s2.size());
         if (dp.find(key) != dp.end()) return dp[key];
 
         if (s1[0] != s2[0]) {
-            auto r1 = calc(s1.substr(1, s1.size() - 1), s2, dp);
+            auto r1 = calc(s1.substr(1, s1.size() - 1), s2);
 
-            if (const auto r2 = calc(s1, s2.substr(1, s2.size() - 1), dp); r1.size() > r2.size()) {
+            if (const auto r2 = calc(s1, s2.substr(1, s2.size() - 1)); r1.size() > r2.size()) {
                 return r1;
             }
         }
 
-        dp[key] = s1[0] + calc(s1.substr(1, s1.size() - 1), s2.substr(1, s2.size() - 1), dp);
+        dp[key] = s1[0] + calc(s1.substr(1, s1.size() - 1), s2.substr(1, s2.size() - 1));
 
         return dp[key];
     }
+
+    unordered_map<string, string> dp {};
 };
 
 
