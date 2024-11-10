0c32779a57fe9938ccd73c3cae8f0bfec9913e4a
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-10 11:44:56 +0800

    c++/1100-1199/1143_longest_common_subsequence.cpp

---
+++ c++/1100-1199/1143_longest_common_subsequence.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        return 0;
+    }
+};
+
+
+int main() {
+    auto s = Solution();
+    cout << s.longestCommonSubsequence("rabbbit", "rabbit") << endl;
+}
