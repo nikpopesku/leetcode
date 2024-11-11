dfd26d27cd78b6f212bf85e6bc190eff8ea8acc5
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-11 11:57:30 +0800

    c++/1000-1099/1035_uncrossed_lines.cpp

---
+++ c++/1000-1099/1035_uncrossed_lines.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+
+    }
+};
+
+
+int main() {
+    auto s = Solution();
+    cout << s.maxUncrossedLines([1,4,2], [1,2,4]) << endl;
+}
