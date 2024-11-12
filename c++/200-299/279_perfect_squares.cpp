2634b7fc25ac1aeadd3b35dfc5a55b0e902e0f7a
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-12 11:52:18 +0800

    c++/200-299/279_perfect_squares.cpp

--- c++/200-299/279_perfect_squares.cpp
+++ c++/200-299/279_perfect_squares.cpp
@@ -1,11 +1,31 @@
-#include <string>
+#include <cmath>
 #include <iostream>
 #include <vector>
+#include <climits>
 
 class Solution {
 public:
     int numSquares(int n) {
-        return n + 1;
+        std::vector dp (n, INT_MAX);
+        dp[0] = 1;
+
+        std::vector<int> squares {};
+
+        for (int i = 1; i < std::sqrt(n); i++) {
+            squares.push_back(i * i);
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (auto sq: squares) {
+                if (sq > n - i) {
+                    break;
+                }
+
+                dp[i] = std::min(dp[i], dp[i-1] + 1);
+            }
+        }
+
+        return dp[n-1];
     }
 };
 
