9f2fec7dfe040ab8046d8ac752cc076c659370b7
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-12 15:26:36 +0800

    c++/500-599/512_coin_change2.cpp

---
+++ c++/500-599/512_coin_change2.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <climits>
+
+class Solution {
+public:
+    int change(int amount, const std::vector<int>& coins) {
+
+    }
+};
+
+
+int main() {
+    auto s = Solution();
+    std::cout << s.change(5, {1,2,5}) << std::endl;
+}
