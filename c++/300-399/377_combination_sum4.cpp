b6f126ebbf95953f149e44b6ea10d4df1674f4fd
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-13 15:15:17 +0800

    c++/300-399/377_combination_sum4.cpp

--- c++/300-399/377_combination_sum4.cpp
+++ c++/300-399/377_combination_sum4.cpp
@@ -2,15 +2,27 @@
 #include <vector>
 #include <climits>
 
+using std::vector;
 
 class Solution {
 public:
-    int combinationSum4(vector<int>& nums, int target) {
+    int combinationSum4(const vector<int>& nums, int target) {
+        vector dp (target+1, 0);
+        dp[0] = 1;
 
+        for (int i = 0; i <= target; i++) {
+            for (auto& n: nums) {
+                if (i >= n) {
+                    dp[i] += dp[i-n];
+                }
+            }
+        }
+
+        return dp[target];
     }
 };
 
 int main() {
     auto s = Solution();
-    std::cout << s.combinationSum4({1,2,3}, 4) << std::endl;
+    std::cout << s.combinationSum4({9}, 3) << std::endl;
 }
