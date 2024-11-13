bde48ded74b0dfebe1fa6a7a9af2eefe4a881dac
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-13 15:06:55 +0800

    c++/300-399/377_combination_sum4.cpp

---
+++ c++/300-399/377_combination_sum4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+    std::cout << s.combinationSum4({1,2,3}, 4) << std::endl;
+}
