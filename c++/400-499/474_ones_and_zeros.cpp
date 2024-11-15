1c453e706d25bcacadc5545d855d236967005dde
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-15 13:35:52 +0800

    c++/400-499/474_ones_and_zeros.cpp

--- c++/400-499/474_ones_and_zeros.cpp
+++ c++/400-499/474_ones_and_zeros.cpp
@@ -9,26 +9,22 @@ using std::max;
 class Solution {
 public:
     int findMaxForm(const vector<string> &strs, const int m, const int n) {
-        vector dp(strs.size() + 1, vector(m + 1, vector(n + 1, 0)));
+        vector dp(m + 1, vector(n + 1, 0));
         int max_value = 0;
 
-        for (int i = 1; i <= strs.size(); i++) {
+        for (int i = 0; i < strs.size(); i++) {
             int zeros = 0;
             int ones = 0;
-            for (const char s0 : strs[i-1]) {
+            for (const char s0 : strs[i]) {
                 if (s0 == '1') ones++;
                 if (s0 == '0') zeros++;
             }
 
-            for (int j = m; j >= 0; j--) {
-                for (int k = n; k >= 0; k--) {
-                    if (j + zeros <= m and k + ones <= n) {
-                        dp[i][j][k] = max(dp[i][j + zeros][k + ones] + 1, dp[i - 1][j][k]);
-                        if (dp[i][j][k] > max_value) {
-                            max_value = dp[i][j][k];
-                        }
-                    } else {
-                        dp[i][j][k] = dp[i - 1][j][k];
+            for (int j = m; j >= zeros; j--) {
+                for (int k = n; k >= ones; k--) {
+                    dp[j][k] = max(dp[j - zeros][k - ones] + 1, dp[j][k]);
+                    if (dp[j][k] > max_value) {
+                        max_value = dp[j][k];
                     }
                 }
             }
