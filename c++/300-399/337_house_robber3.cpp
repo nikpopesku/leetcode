e78161d354a94ba166f99df6566645dde4e346b5
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-15 15:24:16 +0800

    c++/300-399/337_house_robber3.cpp

--- c++/300-399/337_house_robber3.cpp
+++ c++/300-399/337_house_robber3.cpp
@@ -24,5 +24,18 @@ public:
 
 int main() {
     auto s = Solution();
-    cout << s.rob({3,2,3,nullptr,3,nullptr,1}) << endl;
+    auto a = TreeNode(3);
+    auto b1 = TreeNode(2);
+    b1.right = new TreeNode(3);
+
+    auto b2 = TreeNode(3);
+    b1.right = new TreeNode(1);
+
+    a.left = new TreeNode(b1);
+    a.right = new TreeNode(b2);
+
+
+
+//    cout << s.rob({3,2,3,nullptr,3,nullptr,1}) << endl;
+    cout << s.rob(&a) << endl;
 }
