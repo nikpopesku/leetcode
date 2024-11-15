95bb3688bdacb08c08a51931518d0d7082923b7e
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-15 12:40:30 +0800

    c++/400-499/474_ones_and_zeros.cpp

--- c++/400-499/474_ones_and_zeros.cpp
+++ c++/400-499/474_ones_and_zeros.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <vector>
 
-using std::cout, std::endl, std::vector, std::string;
+using std::vector;
+using std::cout, std::endl, std::string;
 
 class Solution {
 public:
     int findMaxForm(const vector<string>& strs, int m, int n) {
+        vector dp (strs.size()+1, vector (m+1, vector(n+1, 0)));
 
+        return 3;
     }
 };
 
